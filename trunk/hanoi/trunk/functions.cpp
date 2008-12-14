4be498b7e6a139a89398222e585b46846b5183d4
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 17:23:40 +0000

    clear screen on invalid input

--- trunk/hanoi/trunk/functions.cpp
+++ trunk/hanoi/trunk/functions.cpp
@@ -11,13 +11,17 @@ int cGetinteger(int base)
  	char mesg[]="How many blocks do you have: ";
 	char str[80];
 	int row,col;
-	long finalVal;
- 	initscr();				/* start the curses mode */
- 	getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
- 	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);     /* print the message at the center of the screen */
-	getstr(str);
-	endwin();
-	finalVal = strtol(str,NULL,base);
+	long finalVal=0;
+	while (finalVal<=0)
+	{
+		clear();				/* clear the screen; use erase() instead? */
+	 	initscr();				/* start the curses mode */
+ 		getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+	 	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);     /* print the message at the center of the screen */
+		getstr(str);
+		endwin();
+		finalVal = strtol(str,NULL,base);
+	}
 	if (finalVal <=0)
 	{
 		cerr << "Please enter a valid integer" << endl;
