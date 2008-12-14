c5044501dffb22ea7175c119dedf28f2ae90f30d
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 18:58:40 +0000

    merge from branch

--- trunk/hanoi/trunk/hanoi.cpp
+++ trunk/hanoi/trunk/hanoi.cpp
@@ -33,12 +33,12 @@ int main (int argc, char *argv[])
 
 void Hanoi(unsigned int n, char source,char dest,char help)
 {
-	if (n==1){printf("Move 1 disk from %c to %c\n",source,dest);}
+	if (n==1){printf("Move disk from %c to %c\n",source,dest);}
 
 	else //if n>1
 	{
 		Hanoi(n-1,source,help,dest);
-		printf("Move 1 disk from %c to %c\n", source, dest);
+		printf("Move disk from %c to %c\n", source, dest);
 		Hanoi(n-1,help,dest,source);
 	}
 }
