34437eb36e97849aefa1230660c4da51dbbe4727
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 00:31:17 +0000

    adding tower of hanoi program

---
+++ trunk/hanoi/trunk/hanoi.cpp
@@ -0,0 +1,39 @@
+/*no box*/
+
+//Includes
+#include <stdio.h>
+#include <string>
+#include "getinteger.cpp"
+/*
+#include "genlib.h"
+#include "simpio.h"
+#include "strlib.h"
+*/
+#include <ctype.h>
+
+//definitions
+
+//prototypes of function
+void Hanoi(unsigned int n, char source,char dest,char help);
+//Create global vars
+unsigned int move;
+
+int main (void)
+{
+	unsigned int n;
+	n = getinteger(10);
+	Hanoi(n, 'a','b','c');
+	return 0;
+}
+
+void Hanoi(unsigned int n, char source,char dest,char help)
+{
+	if (n==1){printf("Move 1 disk from %c to %c\n",source,dest);}
+
+	else //if n>1
+	{
+		Hanoi(n-1,source,help,dest);
+		printf("Move 1 disk from %c to %c\n", source, dest);
+		Hanoi(n-1,help,dest,source);
+	}
+}
