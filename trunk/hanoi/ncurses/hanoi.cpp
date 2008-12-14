e35df030a26071c1d9181f133163dfe596581998
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 18:48:32 +0000

    change the text '1 disk' to just 'disk'

--- trunk/hanoi/ncurses/hanoi.cpp
+++ trunk/hanoi/ncurses/hanoi.cpp
@@ -33,7 +33,7 @@ int main (int argc, char *argv[])
 
 void Hanoi(unsigned int n, char source,char dest,char help)
 {
-	if (n==1){printf("Move 1 disk from %c to %c\n",source,dest);}
+	if (n==1){printf("Move disk from %c to %c\n",source,dest);}
 
 	else //if n>1
 	{
