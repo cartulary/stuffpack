ebd200e5e8bc508c35f885f1a95b928d6139b5be
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 17:17:55 +0000

    Whoa! I forgot at add these; including some (unversioned) cleanup

---
+++ trunk/hanoi/trunk/functions.cpp
@@ -0,0 +1,27 @@
+#include "functions.h"
+#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <iostream>
+
+using namespace std;
+int cGetinteger(int base)
+{
+ 	char mesg[]="How many blocks do you have: ";
+	char str[80];
+	int row,col;
+	long finalVal;
+ 	initscr();				/* start the curses mode */
+ 	getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+ 	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);     /* print the message at the center of the screen */
+	getstr(str);
+	endwin();
+	finalVal = strtol(str,NULL,base);
+	if (finalVal <=0)
+	{
+		cerr << "Please enter a valid integer" << endl;
+		exit(1);
+	}
+	return finalVal;
+}
