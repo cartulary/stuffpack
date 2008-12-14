c18614adb948e33ef982d800890bbb91dc2ee05c
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-14 20:34:07 +0000

    style

--- trunk/hanoi/trunk/hanoi.cpp
+++ trunk/hanoi/trunk/hanoi.cpp
@@ -33,7 +33,10 @@ int main (int argc, char *argv[])
 
 void Hanoi(unsigned int n, char source,char dest,char help)
 {
-	if (n==1){printf("Move disk from %c to %c\n",source,dest);}
+	if (n==1)
+	{
+		printf("Move disk from %c to %c\n",source,dest);
+	}
 
 	else //if n>1
 	{
