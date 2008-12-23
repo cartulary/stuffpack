3a39592ba909875b41b9e1b345931a26b7164e6c
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-23 00:57:21 +0000

    more small fixes and a reminded to self to continue....

--- unn/unn.cpp
+++ unn/unn.cpp
@@ -2,14 +2,25 @@
 #include <vector>
 #include <string.h>
 
+#define FALSE	0
+#define TRUE	1
+
 using namespace std;
 int main(int argc, char* argv)
 {
-      vector<string> files (argv); 
+      vector<string> files; 
 	int i;
-	for (i = 1; i <= argc; i++)
+	bool debug = FALSE;
+	if (argc > 1)
 	{
-		cout << argv[i] << endl;
+		for (i = 1; i <= argc; i++)
+		{
+			if (strcmp((char*)argv[i],"-debug"))
+			{
+				debug = TRUE;
+			}
+			cout << argv[i] << endl;
+		}
 	}
 	return 0;
 }
