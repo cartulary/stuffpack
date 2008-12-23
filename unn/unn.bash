4faa3951b74b9b994740c5870c17ebf92bfadeb6
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-23 01:50:39 +0000

    commit bash version

---
+++ unn/unn.bash
@@ -0,0 +1,27 @@
+#!/usr/local/bin/bash
+debug=false;
+declare -a files;
+while [ $# -gt 0 ];
+do
+	if [ $1 != "-debug" ];
+	then
+		files=("${files[@]}" "$1");
+	else
+		debug=true;
+	fi;
+	shift 1;
+done;
+
+for file in ${files[@]}
+do
+	if [ -e $file ];
+	then
+		echo fatal error: $file exists;
+	else
+		if ($debug);
+		then
+                  echo "starting execution of $file ...";
+                  echo  "we are done";
+		fi;
+	fi;
+done;
