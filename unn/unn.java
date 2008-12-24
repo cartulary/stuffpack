dcecafd4843e4e371ba5263c09993946458d42e8
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-24 02:34:03 +0000

    pretty much done - adding file checking next

--- unn/unn.java
+++ unn/unn.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 public class unn
 {
 	public static void main(String[] args)
 	{
+		Boolean debug = false;
+		ArrayList files = new ArrayList();
+		Iterator filesIt;
+
 		for (String arg : args)
 		{
-			System.out.println(arg);
+			if (arg != "-debug")
+			{
+				files.add(arg);
+			}
+			else
+			{
+				debug = true;
+			}
+		}
+		filesIt = files.iterator();
+		String file;
+		while (filesIt.hasNext())
+		{
+			file = filesIt.next().toString(); 
+			if (debug==true)
+			{
+				System.out.println("starting execution of " + file + " ...");
+				System.out.println("we are done");
+
+			}
+			System.out.println(file);
 		}
    	}
 }
