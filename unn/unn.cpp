3531dbee8aae0e8ee9217ed3a2a220335372bfca
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-24 01:45:45 +0000

    commit final c++ version

--- unn/unn.cpp
+++ unn/unn.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
+#include <unistd.h>
 
 #define FALSE	0
 #define TRUE	1
+#define FILE_READ_OK 0
 
 using namespace std;
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
       vector<string> files; 
 	int i;
 	bool debug = FALSE;
 	if (argc > 1)
 	{
-		for (i = 1; i <= argc; i++)
+		for (i = 1 ; i <= argc - 1 ; i++)
 		{
 			if (strcmp((char*)argv[i],"-debug"))
 			{
+				files.push_back(argv[i]);
+			}
+			else
+			{
 				debug = TRUE;
 			}
-			cout << argv[i] << endl;
+		}
+	}
+	else
+	{
+		return 0;
+	}
+	for (i = 0; i <= files.size() - 1 ; i++)
+	{
+		if (eaccess(files.at(i).c_str(),R_OK) == FILE_READ_OK)
+		{
+			cerr << "fatal error: " << files.at(i) << " exists" << endl;
+		}
+		else
+		{
+			if (debug)
+			{
+                  	cout << "starting execution of " << files.at(i) << " ..." << endl;
+                  	cout << "we are done" << endl;
+			}
 		}
 	}
 	return 0;
