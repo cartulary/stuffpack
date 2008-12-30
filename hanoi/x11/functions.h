51597f1302d25161ce8064c89fb3138c9024b144
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2008-12-30 02:34:40 +0000

    adding x11 branch

---
+++ hanoi/x11/functions.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <iostream>
+
+#include </usr/local/include/xcurses.h>
+
+
+#define ERROR -1
+
+long int getinteger(int);
+
+using namespace std;
