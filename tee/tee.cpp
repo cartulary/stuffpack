b3b428b79f33f2424391c17e4cbd75bda4d557a9
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-01-11 03:33:59 +0000

    we have a better stub for tee; just have to handle files now

--- tee/tee.cpp
+++ tee/tee.cpp
@@ -7,17 +7,39 @@
 
 
 using namespace std;
-
-int main(int argc, const char *argv[])
+bool appendFlag = false, ignoreSignalFlag = false;
+int main(int argc, char *argv[])
 {
-	ofstream file;
-	file.open(argv[1]);
-	std::istream_iterator<char> it(std::cin);
-  	std::istream_iterator<char> end;
-  	std::string results(it, end);
-	cout << results;
-	file.close();
-
+      int c;
+      while ((c = getopt (argc, argv, "ai")) != -1)
+     	{
+            switch (c)
+            {
+                  case 'a':
+                        appendFlag = true;
+                        break;
+                  case 'i':
+                        ignoreSignalFlag = true;
+			default:
+				cerr << "broken" << endl;
+		}
+	}
+      if (argc > 1)
+      {
+            for (int i = optind; i < argc; ++i)
+            {
+			cerr << "we don't handle files yet: " << argv[i] << endl;
+		}
+      }
+	else
+	{
+		string line;
+		while ( ! cin.eof() )
+		{
+			getline(cin, line);
+			cout << line << endl;
+		}
+	}
 	return 0;
 }
 
