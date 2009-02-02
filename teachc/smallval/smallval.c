843502a6ffddbd78baa81c790c958ae88b7fba74
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-02 04:38:26 +0000

    add pg64 E1

--- teachc/smallval/smallval.c
+++ teachc/smallval/smallval.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+static inline unsigned char charMin(unsigned char a, unsigned char b);
 int main (void)
 {
+	unsigned char letters[10];
+	unsigned char smallest = 127;
+	/* get the 10 letters */
+	for(int i = 0; i < 10; ++i)
+	{
+		letters[i] = getchar();
+	}
+	/* check for the smallest */
+	for(int i = 0; i < 10; ++i)
+	{
+		smallest = charMin(smallest, letters[i]);
+	}
+	/* display the smallest value */
+	putchar(smallest);
 	return 0;
+}
+
+/* returns the smallest value of two unsigned chars; look into some portable/reusable function for this*/
+static inline unsigned char charMin(unsigned char a, unsigned char b)
+{
+	return (a < b) ? a : b;
 }
