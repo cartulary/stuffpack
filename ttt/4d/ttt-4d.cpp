5f4096e123e13fdfb90e5fa661cb20b000b6c6d2
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-04 01:51:17 +0000

    so lets upload the 4d stub

---
+++ ttt/4d/ttt-4d.cpp
@@ -0,0 +1,25 @@
+/*********************************************
+* descr: 4d tik tac toe				   *
+* program is licened under the CMU licence   *
+**********************************************/
+const int BOARD_SIZE = 3;
+void displayIntraBoard (char board[BOARD_SIZE][BOARD_SIZE]);
+
+int main(void)
+{
+	return 0;
+}
+
+/************************************************
+ * Name:    displayIntraBoard                 	*
+ * Purpose: display the internal  board       	*
+ * Returns: nothing                           	*
+ * Parameters: 3x3 board			    	*
+ * 	eventually a pointer b/c you can't pass	*
+ *	array							*
+ * Known bugs: 				       	*
+ ***********************************************/
+
+void displayIntraBoard (char board[BOARD_SIZE][BOARD_SIZE])
+{
+}
