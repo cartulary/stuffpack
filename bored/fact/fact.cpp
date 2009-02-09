b7ec680b50d53b9f303b9993338bebcca61283be
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-09 01:37:44 +0000

    commit this for now

---
+++ bored/fact/fact.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main (int argc, char* argv[])
+{
+	unsigned long int num;
+
+	// if the user gave us a value - do something with it
+	if (argc > 1)
+	{
+		num = argv[1];
+	}
+	else
+	{
+	}
+	for
+	return 0;
+}
