54282df7c8580f8d1faae69614046e9f069322c3
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-09 00:59:08 +0000

    add some basic comments

--- unn/unn.php
+++ unn/unn.php
@@ -2,7 +2,9 @@
 <?php
 	$files = array();
 	$debug = false;
+	//eliminate calling command from list
 	array_shift($argv);
+	// loop through the arguments 
 	foreach ($argv as $arg)
 	{
 		if ($arg == '-debug')
@@ -14,7 +16,7 @@
 			$files[] = $arg;
 		}
 	}
-
+	// loop through the files
 	foreach ($files as $file)
 	{
 		if (file_exists($file))
