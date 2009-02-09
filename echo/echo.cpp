852c18847623f92cd0da98ef0a4209bfd6070199
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-09 00:55:20 +0000

    fixed echo

--- echo/echo.cpp
+++ echo/echo.cpp
@@ -8,11 +8,10 @@ int main (int argc, char *argv[])
 	// we are not going to test for flags here - this would cause compat problems */
 	bool noNewlineFlag = false; // do we echo a newline at the end?
 	bool passedFirstN  = false; //set after we passed the first string
-	/*FIXME: I'm sure I could use just one of these bools */
 	for (int i=1; i < argc; ++i)
 	{
 		/* if we have -n and we have yet to pass other chars set flags; else print */
-		if (strcmp (argv[i], "-n") && !passedFirstN)
+		if (strcmp (argv[i], "-n") == 0 && !passedFirstN)
 		{
 			noNewlineFlag = true;
 		}
@@ -21,7 +20,6 @@ int main (int argc, char *argv[])
 			cout << argv[i] << " ";
 			passedFirstN = true;
 		}
-		cout << i;
 	}
 	if (! noNewlineFlag)
 	{
