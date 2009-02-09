b4eecf9e3389b91fbd6e3c2de692e8b1534951c1
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-09 01:07:00 +0000

    add flower box

--- hanoi/text-c/hanoi.c
+++ hanoi/text-c/hanoi.c
@@ -14,7 +14,7 @@ int main (int argc, char *argv[])
 {
 	long n;
 	/* If the amount was passed through a command line argument use it */
-	if (argc > 1) 
+	if (argc > 1)
 	{
 		n = strtol(argv[1],NULL,10);
 	}
@@ -22,15 +22,27 @@ int main (int argc, char *argv[])
 	{
 		n = getinteger(10);
 	}
+
       if (n <=0)
       {
             printf("Please enter a valid integer\n");
 		/* this has to go to std error soon) */
             exit(1);
       }
+
+	//start the program
 	Hanoi(n, 'a','b','c');
 	return 0;
 }
+
+/**********************************************
+ * Name:    Hanoi                             *
+ * Purpose: run the hanoi algorithm           *
+ * Returns: nothing                           *
+ * Parameters: number of times to interate;   *
+ *  char to use for source/dest/help 	    *
+ * Output: steps to complete tower of hanoi   *
+ *********************************************/
 
 void Hanoi(unsigned int n, char source,char dest,char help)
 {
