78874319b6eb62ba2ee9277b3da486cfaa0dcbb7
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-10 03:56:35 +0000

    commit factorial program

--- bored/fact/fact.cpp
+++ bored/fact/fact.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
+char* getcharcters(int);
+
 int main (int argc, char* argv[])
 {
-	unsigned long int num;
+	long int num = 0;
+	unsigned long int factorial = 1;
+	int truearg = argc; //use instead of argc because we modifu it
 
-	// if the user gave us a value - do something with it
-	if (argc > 1)
+	do
 	{
-		num = argv[1];
-	}
-	else
+		// if the user gave us a value - do something with it
+		if (truearg > 1)
+		{
+ 			num = strtol(argv[1],NULL,10);
+		}
+		else
+		{
+		      std::cout << "Please enter a positive integer:" << std::endl;
+     			num = strtol(getcharcters(10),NULL,10);
+      	}
+		truearg = 0; //arguments failed us so try for manual entry
+	} while (num <= 0);
+	// we don't need to multiply by one
+	for (long int i = num; i >= 2; --i)
 	{
+		factorial *= i;
+		std::cout << i << std::endl;
 	}
-	for
+	std::cout << factorial << std::endl;
 	return 0;
 }
+
+char* getcharcters(int max)
+{
+      char* str = (char*)malloc(max*sizeof(int)+1);
+      char ch;
+      int i = 0;
+      
+      while ((ch = getchar()) != '\n' && i < max)
+      {
+            str[i] = ch;
+            i++;
+      }
+      
+      str[max] = NULL; //I hope this is the right way to terminate a char array.
+      return str;
+}
+
