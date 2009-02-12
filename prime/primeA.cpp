071a7015a53c819a29b731762837cbe909c62809
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-12 03:35:58 +0000

    commit small change to output

--- prime/primeA.cpp
+++ prime/primeA.cpp
@@ -5,32 +5,32 @@
 int main (int argc, char *argv[])
 {
 	int toCheck;
+
 	if (argc < 2)
 	{
 		std::cerr << "sorry" << std::endl;
 		return 1;
 	}
+
 	/* get the first integer; maybe check them all eventually? */
 	toCheck = atoi(argv[1]);
+
 	/* we only check until the square root */
 	int maxFactor = floor(sqrt(toCheck));
 	bool isPrime = true;
 
 	for (int i = 2; i <= maxFactor; ++i)
 	{
+		/* check to see if we are prime */
 		if (toCheck % i == 0)
 		{
 			isPrime = false;
+			/* we are not prime; stop checking */
 			break;
 		}
 	}
-	if (isPrime)
-	{
-		std::cout << "prime" << std::endl;
-	}
-	else
-	{
-		std::cout << "not prime" << std::endl;
-	}
+
+	/* print our result -> only print 'npt' if we not prime*/
+	std::cout << ((isPrime) ? "" : "not ") << "prime" << std::endl;
 	return 0;
 }
