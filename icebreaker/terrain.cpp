f62cda1a0345382d1776f306e2835d082c58f551
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-02-15 15:00:04 +0000

    check in stub

---
+++ icebreaker/terrain.cpp
@@ -0,0 +1,18 @@
+class terrain
+{
+		int status;
+		int type;
+		private:
+			void stubFunction();
+		public:
+			void stubFunction2();
+};
+
+void terrain::stubFunction()
+{
+	return;
+}
+void terrain::stubFunction2()
+{
+	return;
+}
