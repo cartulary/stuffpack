74f2b18174583470f66f151a72ad42895333ba2a
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-03-13 03:15:18 +0000

    we want bubble sort to support templates now

--- sorts/bubble/bubble.cpp
+++ sorts/bubble/bubble.cpp
@@ -6,7 +6,8 @@
 const int number_random_objects = 150;
 const int max_random_number = 1000;
 
-std::vector <int> bubbleSort(std::vector <int> toSort);
+template <typename sortReturnType>
+sortReturnType bubbleSort(sortReturnType toSort);
 
 int main (void)
 {
@@ -29,7 +30,8 @@ int main (void)
 	return 0;
 }
 
-std::vector <int> bubbleSort(std::vector <int> toSort)
+template <typename sortReturnType>
+sortReturnType bubbleSort(sortReturnType toSort)
 {
 	bool swapped;
 	int len = toSort.size();
