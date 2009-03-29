130ae5bc7502fba8fe1c6a8b2870cdb08130a12a
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-03-29 19:13:13 +0000

    now it pretty much acts like the others

--- hanoi/fltk/main.cpp
+++ hanoi/fltk/main.cpp
@@ -4,7 +4,16 @@
 #include <stdlib.h>
 #include "../hanoi.cpp"
 #include "inputWindow.cpp"
+
+int getNumberOfBlocks(int argc, char* argv[]);
+
 int main(int argc, char* argv[])
+{
+	Hanoi(getNumberOfBlocks(argc, argv),'a','b','c');
+	return 0;
+}
+
+int getNumberOfBlocks(int argc, char* argv[])
 {
 	SliderWindow pickNumberWindow("Pick a number");
 	pickNumberWindow.resetHanoi();
@@ -13,19 +22,18 @@ int main(int argc, char* argv[])
 	while (Window::first())
 	{
 		wait();
-		hanoiVal = pickNumberWindow.getHanoi();
-		if (hanoiVal > 0)
-		{
-			/*
-				Since we are moving toward making this one unified system
-				lets tru and get this to be like the other versions.
-				Only ask once.  Once we have answer get out of the loop.
-			*/
-			break;
-		}
-	}
-	Hanoi(hanoiVal,'a','b','c');
-	pickNumberWindow.resetHanoi();
-
-	return 0;
+            hanoiVal = pickNumberWindow.getHanoi();
+            if (hanoiVal > 0)
+            {
+                  /*
+                        Since we are moving toward making this one unified system
+                        lets tru and get this to be like the other versions.
+                        Only ask once.  Once we have answer get out of the loop.
+                  */
+			return hanoiVal;
+            }
+      }
+	//not really needed because we return before its called but I'll leave it
+      pickNumberWindow.resetHanoi();
 }
+
