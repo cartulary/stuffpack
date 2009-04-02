ccb12765363c37577cd954e73c0358e52d42a8bb
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-04-02 01:09:01 +0000

    Why is ./wc != wc ?

--- wc/wc.cpp
+++ wc/wc.cpp
@@ -54,6 +54,7 @@ int main(int argc, char *argv[])
 	{
 		charFlag = lineFlag = byteFlag = wordFlag = true;
 	}
+
 	/* If we have files on command line - display them; else just display cin once.*/
 	if (argc > 1)
 	{
@@ -106,17 +107,20 @@ void doWcFile (istream &toWc)
 	unsigned long int charNums = 0;
 	unsigned long int words = 1; /* begin at one because we look for spaces; not words */
 	unsigned long int longestLine = 0;
+
 	/* the static content - used for 'total' */
 	static unsigned long int sLineNums = 0;
 	static unsigned long int sCharNums = 0;
 	static unsigned long int sWords = 1;
 	static unsigned long int sLongestLine = 0;
+
 	bool lastSpace = false; /* use so double spaces are not counted as words */
 	bool curCharIsSpace = false;
 	string line;
 
 	while (! toWc.eof() )
 	{
+//		lastSpace = false;
 		getline(toWc, line);
 		/* does not deal with -c vs -m yet! */
 		unsigned int lineLen = line.size();
@@ -130,11 +134,10 @@ void doWcFile (istream &toWc)
 
 		for(unsigned int i = 0; i < lineLen; ++i)
 		{
-			/* use iswspace? */
 			/* we are not going to use .at() because we know we are in range */
-			curCharIsSpace = iswspace(line[i]);
 			if (iswspace(line[i]))
 			{
+				lastSpace = true;
 				if (!lastSpace)
 				{
 					++words;
