a85fed12f5b4517485f4c453de0cca79949f502d
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-04 23:04:21 +0000

    newest version of the script

--- symlink_dir/script.sh
+++ symlink_dir/script.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-cd /usr/ports/
-for main_cat in $(make -V SUBDIR);
+portdir="/usr/ports"
+for main_cat in $(make -V SUBDIR -C $portdir);
 do
-	if [ -d $main_cat ];
+	if [ -d $portdir/$main_cat ];
 	then
-		for port in $(make -V SUBDIR -C $main_cat);
+		for port in $(make -V SUBDIR -C $portdir/$main_cat);
 		do
-			echo "$main_cat/$port"
+			echo -n "$main_cat/$port:";
+			for item_cat in $(make -V CATEGORIES -C $portdir/$main_cat/$port);
+			do
+				echo -n "$item_cat "
+				if [ ! -d $item_cat ];
+				then
+					mkdir $item_cat;
+				fi;
+			done;
+			echo
 		done;
 		echo $main_cat
 	fi;
