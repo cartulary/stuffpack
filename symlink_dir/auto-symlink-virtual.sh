d4462f1ed39fa75ff55fffde1a052ff5ee1d9091
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-05 01:14:33 +0000

    add another check for correct dir

--- symlink_dir/auto-symlink-virtual.sh
+++ symlink_dir/auto-symlink-virtual.sh
@@ -1,28 +1,40 @@
 #!/usr/bin/env sh
-
 portdir="/usr/ports"
+while getopts dp: option
+do    case "$option" in
+      'd')  set -x;;
+      'p')  portdir=$OPTARG;;
+      '?')  echo "Usage: $0 [-d] [-pPortdir]" >&2;
+            exit 1;;
+      esac
+done
+
+
 for main_cat in $(make -V SUBDIR -C $portdir);
 do
 	if [ -d $portdir/$main_cat ];
 	then
 		for port in $(make -V SUBDIR -C $portdir/$main_cat);
 		do
-			echo -n "$main_cat/$port:";
-			for item_cat in $(make -V CATEGORIES -C $portdir/$main_cat/$port);
-			do
-				echo -n "$item_cat "
-				if [ ! -d $item_cat ];
-				then
-					mkdir $item_cat;
-				fi;
-				if [ ! -e $item_cat/$port ];
-				then
-					#this won't create the main ports because we check to see if the main port one exits BUT
-					#we add the main cat to the filename in case of name conflicts
-					ln -s $portdir/$main_cat/$port  $item_cat/$port-$main_cat
-				fi;
-			done;
-			echo
+			if [ -d $portdir/$main_cat/$port ];
+			then
+				echo -n "$main_cat/$port:";
+				for item_cat in $(make -V CATEGORIES -C $portdir/$main_cat/$port);
+				do
+					echo -n "$item_cat "
+					if [ ! -d $item_cat ];
+					then
+						mkdir $item_cat;
+					fi;
+					if [ ! -e $item_cat/$port ];
+					then
+						#this won't create the main ports because we check to see if the main port one exits BUT
+						#we add the main cat to the filename in case of name conflicts
+						ln -s $portdir/$main_cat/$port  $item_cat/$port-$main_cat
+					fi;
+				done;
+				echo
+			fi;
 		done;
 		echo $main_cat
 	fi;
