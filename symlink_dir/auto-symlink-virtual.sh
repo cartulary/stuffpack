fbb7cc721d45d634fa46f7a86ddd392c273e74fe
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-07 01:18:13 +0000

    add some patches - and make the script work with whitespace

--- symlink_dir/auto-symlink-virtual.sh
+++ symlink_dir/auto-symlink-virtual.sh
@@ -41,17 +41,17 @@ done
 do_link_port()
 {
 	[ -n "$verbose" ] && echo "  " ln -s "$1" "$2";
-	[ -z "$dryrun" ] && ln -s $1 $2;
+	[ -z "$dryrun" ] && ln -s "$1" "$2";
 
 }
 
 do_make_cat_dir() 
 {
-	if [ ! -d $whereto/$1 ];
+	if [ ! -d "$whereto/$1" ];
 	then
-		[ -n "$verbose" ] && echo "  " mkdir -p $whereto/$1;
-		[ -z "$dryrun" ] && mkdir -p $whereto/$1;
-	fi;
+		[ -n "$verbose" ] && echo "  " mkdir -p "$whereto/$1";
+		[ -z "$dryrun" ] && mkdir -p "$whereto/$1";
+		fi;
 }
 
 op_port()
@@ -60,7 +60,7 @@ op_port()
 	item_cat=$2;
 	portname=$3;
 	portcat=$4;	
-	do_make_cat_dir $item_cat;
+	do_make_cat_dir "$item_cat";
 	if [ "$item_cat" = "$portcat" ]
 	then dest="$whereto/$item_cat/$portname";
 	else dest="$whereto/$item_cat/$portname-$portcat";
@@ -78,7 +78,7 @@ transverse_sub_tree()
 	for portname in $(make -V SUBDIR -C $1/$2);
 	do
 		portpath="$1/$2/$portname";
-		if [ -d $1/$2/$portname ];
+		if [ -d "$1/$2/$portname" ];
 		then
 			echo -n "$2/$portname:";
 			for item_cat in $(make -V CATEGORIES -C $1/$2/$portname);
@@ -96,22 +96,22 @@ transverse_tree()
 {
 	for main_cat in $(make -V SUBDIR -C $1);
 	do
-		if [ -d $1/$main_cat ];
+		if [ -d "$1/$main_cat" ];
 		then
 			[ -n "$verbose" ] && echo "Transversing $main_cat";
-			transverse_sub_tree $1 $main_cat
+			transverse_sub_tree "$1" "$main_cat"
 		fi;
 	done;
 }
 
 transverse_index()
 {
-	cat $1/$index | while read LINE;
+	cat "$1/$index" | while read LINE;
 	do
 		portpath=$(echo "$LINE" | awk -F\| '{print $2}')
 		portcats=$(echo "$LINE" | awk -F\| '{print $7}')
-		portname=$(basename $portpath)
-		portcat=$(basename $(dirname $portpath))
+		portname=$(basename "$portpath")
+		portcat=$(basename $(dirname "$portpath"))
 		echo "$portname: $portcats"
 		for item_cat in $portcats;
 		do
@@ -120,7 +120,7 @@ transverse_index()
 	done
 }
 
-if [ ! -d $portdir ];
+if [ ! -d "$portdir" ];
 then
 	echo "Complete failure";
 	exit 1;
@@ -128,19 +128,19 @@ fi;
 
 if [ -z "$index" ];
 then
-	if [ ! -e $portdir/Makefile ];
+	if [ ! -e "$portdir/Makefile" ];
 	then
 		echo "No Makefile";
 		exit 1;
 	fi;
-	transverse_tree $portdir;
+	transverse_tree "$portdir";
 else
-	if [ ! -e $portdir/$index ];
+	if [ ! -e "$portdir/$index" ];
 	then
 		echo "No Index";
 		exit 1;
 	fi;
-	transverse_index $portdir;
+	transverse_index "$portdir";
 fi;
 
 return 0;
