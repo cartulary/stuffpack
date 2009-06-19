70b46133c5564743ddacaeb046d2b56ff27074dc
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-19 21:18:12 +0000

    more into functions; remove hard limit of number of cols

--- tdb/readdb.sh
+++ tdb/readdb.sh
@@ -1,18 +1,51 @@
 #!/usr/bin/env sh
-cat db | while read LINE;
+
+db=db
+
+usage()
+{
+	base=$(basename $0);
+	echo "$base: metadata index for ports";
+	echo;
+	echo "Usage: $base [-fxV]";
+	echo
+}
+
+while getopts f:xV option
 do
-	if [ -n "$LINE" ];
-	then
-		echo -n "$LINE" "-->" ;
-		c=1;
-		while [ $c -le 5 ];
-		do
-			result=$(echo "$LINE" | awk -F\| "{print \$$c}");
-			item=$(echo $result | awk -F= "{print $1}");
-			value=$(echo $result | awk -F= "{print $1}");
-			echo -n " $item is $value";
-			c=$(($c+1));
-		done;
-		echo;
-	fi;
-done;
+	case "$option" in
+		'f') db=$OPTARG;;
+		'x') set -x;;
+		'V') echo "Version pre-alpha";
+			exit 0;;
+		'?') usage; exit 1;
+	esac
+done
+
+op_db_line()
+{
+}
+
+loop_through_db()
+{
+	cat $db | while read LINE;
+	do
+		if [ -n "$LINE" ];
+		then
+			echo -n "$LINE" "-->" ;
+			c=1;
+			max_col=$(echo $LINE|sed "s/[^|]//g"|wc -c);
+			while [ $c -le $max_col ];
+			do
+				result=$(echo "$LINE" | awk -F\| "{print \$$c}");
+				item=$(echo $result | awk -F= "{print $1}");
+				value=$(echo $result | awk -F= "{print $1}");
+				echo -n " $item is $value";
+				c=$(($c+1));
+			done;
+	echo;
+		fi;
+	done;
+}
+
+loop_through_db;
