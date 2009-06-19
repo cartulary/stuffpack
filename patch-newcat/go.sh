02694e3d78857f494881e8f484b392ba469814ab
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-19 01:52:40 +0000

    add the basic script

---
+++ patch-newcat/go.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+cd new
+cat "INDEX-7" | while read LINE;
+
+do
+	portpath=$(echo "$LINE" | awk -F\| '{print $2}');
+	portname=$(basename "$portpath");
+	it=$(echo "$portname [$portpath]"|grep -E "(i18n)|(l10n)");
+	if [ -n "$it" ];
+	then
+		echo $it;
+		if [ -d $portpath ];
+		then
+			echo $(make -C $portpath -V CATEGORIES);
+		fi;
+	fi;
+
+done
