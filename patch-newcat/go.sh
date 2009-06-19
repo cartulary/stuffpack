7a412ead0a6b378be8ef44ce335fbc182934c922
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-19 02:27:53 +0000

    I have everything - except that I am not keeping my sed and I am not echoing correctly with awk

--- patch-newcat/go.sh
+++ patch-newcat/go.sh
@@ -10,10 +10,12 @@ do
 		echo $it;
 		if [ -d $portpath ];
 		then
+			echo old: $(make -C $portpath -V CATEGORIES);
 			linenum=$(cat $portpath/Makefile | grep -n CATEGORIES);
 			linenum=$(echo $linenum|awk -F: '{print $1}');
 			echo $linenum;
+			cmd=$linenum"s/\$/ i18n/"
+			echo $(cat $portpath/Makefile|sed "$cmd"|awk "NR==$linenumm{print \$1;}");
 		fi;
 	fi;
-
 done
