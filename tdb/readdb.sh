0b0eced0ce7a529713c39e01a3a7b7c058d33249
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-06-19 20:56:50 +0000

    make a whole bunch of changes

--- tdb/readdb.sh
+++ tdb/readdb.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 cat db | while read LINE;
 do
-	echo -n $LINE "-->" ;
-	c=1;
-	while [ $c -le 5 ];
-	do
-		result=$(echo "$LINE" | awk -F\| "{print \$$c}");
-		echo -n $result;
-		c=$(($c+1));
-	done;
-	item=$(echo "$LINE" | awk -F\| "{print \$1}");
-	a=$(echo "$LINE" | awk -F\| "{print \$2}");
-	b=$(echo "$LINE" | awk -F\| "{print \$3}");
-	c=$(echo "$LINE" | awk -F\| "{print \$4}");
-	echo -n $a $b $c;
-	echo;
+	if [ -n "$LINE" ];
+	then
+		echo -n "$LINE" "-->" ;
+		c=1;
+		while [ $c -le 5 ];
+		do
+			result=$(echo "$LINE" | awk -F\| "{print \$$c}");
+			item=$(echo $result | awk -F= "{print $1}");
+			value=$(echo $result | awk -F= "{print $1}");
+			echo -n " $item is $value";
+			c=$(($c+1));
+		done;
+		echo;
+	fi;
 done;
