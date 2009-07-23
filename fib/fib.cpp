2671c1cdf25784f5aaeebe1fc0fc92acb9f9faca
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-07-23 01:50:58 +0000

    get the sum

--- fib/fib.cpp
+++ fib/fib.cpp
@@ -6,12 +6,14 @@ int main (void)
 	double tmp;
 	double sum = 0;
 	int i;
-	for (int i = 0; i <= 75; ++i)
+	for (i = 1; i <= 75; ++i)
 	{
 		tmp = first_num;
 		first_num = next_num;
 		next_num = first_num + tmp;
-		std::cout << std::fixed << first_num << std::endl;
+		sum += first_num;
+		std::cout << std::fixed << first_num << "(" << i << ")" <<std::endl;
 	}
+	std::cout << std::fixed << sum << std::endl;
 	return 0;
 }
