924af88c4a80fa3155e4d480df8c4d060500f9e5
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-10-23 13:09:17 +0000

    Commit next version

--- class/star-problems/star.cpp
+++ class/star-problems/star.cpp
@@ -8,21 +8,34 @@
  * notes: this could be done better...	    *
  *********************************************/
 
+#include <getopt.h>
 #include <iostream>
 
 void repeat_char(const char* to_print, int times);
 
+extern char* optarg;
+
 int main (int argc, char* argv[])
 {
-	if (argc < 2)
+	long rows = 2;
+	int c;
+	const char* diamond_char= "*";
+	while (( c = getopt (argc, argv, "c:n:")) != -1)
 	{
-		std::cout << "Usage: Please choose a value..." << std::endl;
-		return 1;
+		switch (c)
+		{
+			case 'c':
+				diamond_char=optarg;
+				break;
+			case 'n':
+				rows = strtol(optarg, NULL, 10);
+				break;
+			default:
+				break;
+		}
 	}
-
 	// We want the first command line option; argv[0] == name thaat program is executed with
-	long rows = strtol(argv[1], NULL, 10);
-	int cols;
+	int cols =1;
 
 	/* because we use repeat_char we don't need a col counter */
 	int row;
@@ -33,10 +46,9 @@ int main (int argc, char* argv[])
 	// Odd values with <= will print extra line; lets see if we could fix this.
 	for (row = 0; row <= rows ; ++row)
 	{
-
 		int space_cols = (rows/2) - (cols /2);
 		repeat_char(" ", space_cols);
-		repeat_char("*", cols);
+		repeat_char(diamond_char, cols);
 		std::cout << std::endl;
 		cols += amt_to_add;
             if (row +1 > (rows /2) -1)
