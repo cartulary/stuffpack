473ca58f84a0723bc612a3cadbd056ac334efee9
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-10-24 15:57:28 +0000

    I think nearly all remaining details have been worked out...; just the multi-char strings to deal with

--- class/star-problems/star.cpp
+++ class/star-problems/star.cpp
@@ -3,7 +3,7 @@
  * Purpose: Print a diamon using N rows	    *
  * Returns: 0 when argument exists; else 1    *
  * Parameters: N as first command line option *
- * Known bugs: extra line printed with odd n  *
+ * Known bugs: 					    *
  * algo for spacing ignores # of chars in -c  *
  * compatibility:	N/A				    *
  * notes: this could be done better...	    *
@@ -15,6 +15,7 @@
 void repeat_char(const char* to_print, int times);
 void usage(char* prog);
 
+// Created by the getopt library
 extern char* optarg;
 
 int main (int argc, char* argv[])
@@ -22,6 +23,7 @@ int main (int argc, char* argv[])
 	long rows = 0;
 	int c;
 	const char* diamond_char= "*";
+	// Lets get the command line arguments
 	while (( c = getopt (argc, argv, "c:n:")) != -1)
 	{
 		switch (c)
@@ -30,6 +32,7 @@ int main (int argc, char* argv[])
 				diamond_char=optarg;
 				break;
 			case 'n':
+				// I think I should do some sanity testing here...
 				rows = strtol(optarg, NULL, 10);
 				break;
 			default:
@@ -48,7 +51,7 @@ int main (int argc, char* argv[])
 	{
 		// if we repeat the middle line it looks weird; if we don't then it also looks weird
 		// I'm tempted to not even allow even sized diamonds
-		std::cout << "An even sized diamond is ambiguous..." << std::endl;
+		std::cerr << "An even sized diamond is ambiguous..." << std::endl;
 	}
 	// We want the first command line option; argv[0] == name thaat program is executed with
 	int cols =1;
@@ -63,9 +66,23 @@ int main (int argc, char* argv[])
 	for (row = 0; row < rows ; ++row)
 	{
 		int space_cols = (rows/2) - (cols /2);
-		repeat_char(" ", space_cols);
-		repeat_char(diamond_char, cols);
-		std::cout << std::endl;
+		if (cols > 0)
+		{
+			// If cols == 0 (the last line of even rows) don't print anything
+			repeat_char(" ", space_cols);
+			repeat_char(diamond_char, cols);
+			std::cout << std::endl;
+		}
+
+		if (row+1 == (rows / 2) && (rows % 2) == 0)
+		{
+			//repeat the row if we have an even number of rows
+			// and we are in the middle.
+			repeat_char(" ", space_cols);
+			repeat_char(diamond_char, cols);
+			std::cout << std::endl;
+			amt_to_add=-2;
+		}
 		cols += amt_to_add;
             if (row +1 > (rows /2) -1)
             {
@@ -75,6 +92,14 @@ int main (int argc, char* argv[])
 	return 0;
 }
 
+/**********************************************
+ * Name:    repeat_char                       *
+ * Purpose: print specific char[] N times     *
+ * Parameters: to_print (char[]), times (N)   *
+ * Known bugs: none      			    *
+ * notes: start from 1; use "<=" for	    *
+ *	readability reasons			    *
+ *********************************************/
 void repeat_char(const char* to_print, int times)
 {
 	for (int counter = 1; counter <= times; ++counter)
@@ -83,10 +108,16 @@ void repeat_char(const char* to_print, int times)
 	}
 }
 
+/**********************************************
+ * Name:    usage                             *
+ * Purpose: Prints usage information to stderr*
+ * Parameters: the name of the program	    *
+ *	(always pass argv[0])			    *
+ *********************************************/
 void usage(char* prog)
 {
-	std::cout << "usage: "  << prog << " -n rows [-c string ]" << std::endl;
-	std::cout << std::endl;
-	std::cout << "\t -n \t number of rows to print in the diamond" << std::endl;
-	std::cout << "\t -c \t what character/string to print diamond with " << std::endl;
+	std::cerr << "usage: "  << prog << " -n rows [-c string ]" << std::endl;
+	std::cerr << std::endl;
+	std::cerr << "\t -n \t number of rows to print in the diamond" << std::endl;
+	std::cerr << "\t -c \t what character/string to print diamond with " << std::endl;
 }
