60e9be3429ccae707a778dfb4e8e67d6b828a0e0
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-10-27 12:25:05 +0000

    watch gains some new features

--- watch-for-freebsd/watch.sh
+++ watch-for-freebsd/watch.sh
@@ -4,10 +4,11 @@ usage()
 	base=$(basename $0);
 	echo "$base: watch a command run multiple times";
 	echo "";
-	echo "Usage: $base [-dV] [-s seconds] -c cmd";
+	echo "Usage: $base [-dV] [ -n times ] [-s seconds] -c cmd";
 	echo "";
 	echo "-c		What command to run";
 	echo "-d		Debugging mode";
+	echo "-n		Run the command N times";
 	echo "-V		Print version information then exit";
 	echo "-s		Time to delay between command runs";	
 	echo "";
@@ -15,15 +16,21 @@ usage()
 #set the default values
 seconds="1";
 cmd="";
+# by default we go forever; but we use -lt later on so we need to set this
+ntimes=1;
+# we use this instead of checking ntimes...
+forever="YES";
 
 #lets set the variables here
 #Why can't we get cmd to not require -c ??
-while getopts c:ds:V option
+while getopts c:dn:s:V option
 do	case "$option" in
 	'c') cmd="$OPTARG" ;;
 	'd') set -x;;
+	'n')	ntimes=$OPTARG;
+		forever="";;
 	's') seconds="$OPTARG";;
-	'V') echo "Version 0.1";
+	'V') echo "Version 0.2";
 		exit 0;;
 	'?') usage;
 		exit 1;;
@@ -39,12 +46,18 @@ then
 fi;
 
 #lets run the command; sleep; run the command; sleep etc
-while	[ 1 ]
+count=0;
+while	[ $count -lt $ntimes ]
 do
 	$cmd;
 	sleep $seconds;
+
+	# we only increment counter if we want to stop...
+	if [ -z $forever ]
+	then
+		count=$(($count + 1));
+	fi
 done
 
-#feature request: make the number of the command is run choosable
 #feature request: verify this implentation against the Linux version
 #???		    : Is signal proccessing correct?
