7b1d0bfec348b9eb77206c5096ded37faca6da8f
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-01 16:34:32 +0000

    lets make this into a usable script

--- gapi/startwork.php
+++ gapi/startwork.php
@@ -1,5 +1,4 @@
 #!/usr/bin/env php
-
 <?php
 	require_once 'config.php';
 	require_once 'Zend/Loader.php';
@@ -10,5 +9,36 @@
 	$client = Zend_Gdata_ClientLogin::getHttpClient($email, $password, Zend_Gdata_Gapps::AUTH_SERVICE_NAME);
 	$service = new Zend_Gdata_Gapps($client, $domain);
 
-	$service->retrieveUser("eitana");
+	$shortopts = "";
+	$shortopts .= "u:";	//username [ required ]
+	$shortopts .= "n:";	//full name (first last), default: (Scardy Cat)
+	$shortopts .= "p:";	//password [ required ] 
+	$shortopts .= "h";	//print usage info
+
+	$cli = getopt($shortopts);
+
+	if (!is_null($cli['h']))
+	{
+		usage();
+	}
+
+	$username = (empty($cli['u'])) ? '' : $cli['u'];
+	$fullname = (empty($cli['n'])) ? '' : $cli['n'];
+	$password = (empty($cli['p'])) ? '' : $cli['p'];
+
+	if (empty($username) || empty($fullname) || empty($password)
+	{
+		usage();
+	}
+
+	// split full name into given and family name
+	
+	$name = explode(" ", $fullname, 2);
+
+	$service->createUser($username, $name[0], $name[1], $password, null, null)
+	function usage()
+	{
+		echo "Usage: -u[sername] username -n[ame] 'full name' -p[assword] password\n";
+		die();
+	}
 ?>
