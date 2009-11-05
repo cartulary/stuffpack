3f037795436573d4978182b0a7b7acb55e41cdf8
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-05 21:14:41 +0000

    Add the stats info; I think the game works

--- craps/craps.cpp
+++ craps/craps.cpp
@@ -21,14 +21,15 @@ int main (int argc, char* argv[])
 {
 	srand(time(0));
 
+	int shooterMain;
 	// go 100 times
-	for (int count = 1; count <= 10000; ++count)
+	for (int count = 1; count <= 1000; ++count)
 	{
 	//	std::cin.clear();
 	//	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 //		int shooterMain;
 //		std::cin >> shooterMain;
-		int shooterMain = 7;
+		shooterMain = 7;
 		int roll = rollDice(2);
 		int chance =0;
 		//std::cout << count << " (" << roll << ") \n";
@@ -98,7 +99,36 @@ int main (int argc, char* argv[])
 					}
 		}
 	}
-	std::cout << "Win:" <<win("")<< " " << "Lose: " << lose("") << std::endl;
+	int didWin = win(""); // we start at -1 so don't worry about incrementing counter
+	int didLose = lose("");
+	std::cout << "Win:" <<didWin<< " " << "Lose: " << didLose << std::endl;
+	double chance = (double)didWin/(didWin+didLose);
+	double realChance, realDisadvantage;
+	switch(shooterMain)
+	{
+		case 5:
+			realChance = 0.492;
+			realDisadvantage = 1.52;
+			break;
+		case 6:
+			realChance = 0.488;
+			realDisadvantage = 2.34;
+			break;
+		case 7:
+			realChance = 0.493;
+			realDisadvantage = 1.41;
+			break;
+		case 8:
+			realChance = 0.488;
+			realDisadvantage = 2.34;
+			break;
+		case 9:
+			realChance = 0.492;
+			realDisadvantage = 1.52;
+			break;
+	}
+	std::cout << "Your chance of winning was " << chance << "This is " << ((chance<realChance) ? "less" :  (chance>realChance) ? "more" : "equal" ) << " than the real chance " << realChance << "\n";
+	std::cout << "Your disadvantage is" << realDisadvantage << std::endl;
 	return 0;
 }
 
