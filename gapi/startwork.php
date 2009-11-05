eb83371cc7ead19d08688cc992bf4d6601827c02
eitan.adler <eitan.adler@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-05 18:37:51 +0000

    add change-name; fix some errors; add more coloring to the script

--- gapi/startwork.php
+++ gapi/startwork.php
@@ -3,6 +3,8 @@
 	// All of these are set in config.php (and thats it)
 	// $config['user password domain']
 	require_once 'config.php';
+	// set the email right here.
+	$config['email']= $config['user'].'@'.$config['domain'];
 	require_once 'Zend/Loader.php';
 	Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
 	Zend_Loader::loadClass('Zend_Gdata_Gapps');
@@ -27,6 +29,10 @@
 		"display" => Array (
 			"required_opts" => Array('u'),
 			"help" => "-u username"
+		),
+		"change-name" => Array(
+			"required_opts" => Array('u','f','l'),
+			"help" => "-u username -f first name -l last name"
 		),
 		"update" => Array(
 			"enabled" => false,
@@ -56,7 +62,7 @@
 	$shortopts .= "p:";	//password 
 	$shortopts .= "h";	//print usage info
 
-	$longopts = Array ("action:", 'debug');
+	$longopts = Array ("action:", 'debug', 'verbose','check-config');
 
 	$cli = getopt($shortopts, $longopts);
 	if (!is_null($cli['h']))
@@ -64,15 +70,30 @@
 		usage();
 	}
 
+	//convert from getopt moe to useable mode...
 	$doWhat = (empty($cli['action'])) ? '' : $cli['action'];
-	echo "Trying to $doWhat...\n";
+	$doWhatPrint = colorText(32,$doWhat);
+	echo "Trying to $doWhatPrint...\n";
 	$username = (empty($cli['u'])) ? '' : $cli['u'];
 	$name[0] = (empty($cli['f'])) ? '' : $cli['f'];
 	$name[1] = (empty($cli['l'])) ? '' : $cli['l'];
 	$givepass = (empty($cli['p'])) ? '' : $cli['p'];
 	$nickname = (empty($cli['n'])) ? '' : $cli['n'];
 
+	// long opts
 	$debug = (array_key_exists('debug',$cli)) ? true : false;
+	$verbose = (array_key_exists('verbose',$cli)) ? true : false;
+	$check_config = (array_key_exists('check-config',$cli)) ? true : false;
+
+	// act on the long opts
+	if ($check_config)
+	{
+		$email = colorText(1,$config['email']);
+		$password = colorText(1,$config['password']);
+		echo "Please verify that $email is your email address and that $password is your password\n";
+		// we don't go farther than this with check-config
+		return;
+	}
 
 	if ($debug)
 	{
@@ -84,7 +105,8 @@
 		//show usage...
 		usage("dowhat not in array");
 	}
-	if ($actions[$doWhat]["enabled"] == false)
+	// triple === for null checking
+	if ($actions[$doWhat]["enabled"] === false)
 	{
 		usage("This action is not enabled");
 	}
@@ -97,12 +119,10 @@
 		}
 	}
 
-	$config['email']= $config['user'].'@'.$config['domain'];
 	$client = Zend_Gdata_ClientLogin::getHttpClient($config['email'], $config['password'], Zend_Gdata_Gapps::AUTH_SERVICE_NAME);
 	$service = new Zend_Gdata_Gapps($client, $config['domain']);
 
-
-	echo "Editing user: $username\n";
+	echo "Editing user:".colorText(32,$username)."\n";
 
 	// I think we should move to getting the user object before the case and submitting it after the case
 	// This should make it easier to do certain things (like updating an account).
@@ -112,10 +132,6 @@
 		switch ($doWhat)
 		{
 			case 'add':
-				if (empty($name[0]) || empty($name[1]))
-				{
-					usage();
-				}
 				$service->createUser($username, $name[0], $name[1], $givepass, null, null);
 				break;
 			case 'del':
@@ -123,10 +139,30 @@
 				break;
 			case 'suspend':
 				$service->suspendUser($username);
+				break;
 			case 'restore':
 				$service->restoreUser($username);
+				break;
 			case 'display':
 				displayUser($username);
+				break;
+			case 'change-name':
+				if ($verbose)
+				{
+					echo colorText(1,"Before"),":\n";
+					displayUser($username);
+				}
+				$user = $service->retrieveUser($username);
+				$user->name->givenName = $name[0];
+				$user->name->familyName = $name[1];
+				$user = $user->save();
+				if ($verbose)
+				{
+					echo colorText(1,"After"),":\n";
+					displayUser($username);
+				}
+				break;
+
 				
 		}			
 		echo 	"This appears to have worked...\n";
@@ -148,10 +184,11 @@
 		exit();
 	}
 
-	function usage($where="")
+	function usage($message="")
 	{
-		global $actions;
-		$me = basename ( __FILE__ );
+		global $actions, $doWhat;
+		$me = colorText(1,basename ( __FILE__ ));
+		echo "This file is called $me\n";
 		foreach ($actions as $key => $params)
 		{
 			$helpline = $params['help'];
@@ -166,9 +203,17 @@
 				echo $params['descr'] . "\n\t";
 			}
 			$key_print = colorText(1,$key);
-			echo "$sur_text $key_print: $me -a $action $helpline\n";
+			// Indicate which action we want to use";
+			if ($doWhat == $key)
+			{
+				$key_print = colorText(32,$key);
+				$helpline = colorText(32,$helpline);
+			}
+			echo "$sur_text --action $key_print $helpline\n";
 		}
-		if (!empty($message)) {echo "Message: $where\n";}
+		echo colorText(1," --check-config")."\t Verifies that your configuration options are correct\n";
+		if (!empty($message)) {echo "Message: $message\n";}
+		echo "Please send all bug reports to Eitan Adler\n";
 		die();
 	}
 
