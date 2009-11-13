d05f093467be1b770f0009e7ea2bc661bf91e0e2
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-13 12:57:17 +0000

    fix comment

--- fib/fib.cpp
+++ fib/fib.cpp
@@ -33,7 +33,9 @@ mpz_class getFibN(long nthFib)
 		// Otherwise we end up with a segfault later on (fibMap[1]...)
 		return 0;
 	}
-	// since we want to deal with the users idea of numbers ignore fibMap[0]
+	/* we store the data in a map because this way we
+		don't recalculate the same value multiple times */
+
 	mpz_class *fibMap = new mpz_class[nthFib + 1];
 
 	int i;
