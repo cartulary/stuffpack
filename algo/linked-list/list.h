944bd99d804722a5c5d25f419ced91d7a11468fe
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-15 08:46:21 +0000

    add some more comments - try to plan this out before I write it

--- algo/linked-list/list.h
+++ algo/linked-list/list.h
@@ -1,14 +1,23 @@
 class list
 {
 	public:
+		/* link to next item in chain */
 		list* next;
+		/* link to prev item in chain */
 		list* prev;
 
+		/* first item in chain - useful? */
+		static list* head;
+
+		/* well - the most important part*/
 		int data;
 
+		/* adds item right after this node?? */
 		list* add();
+		/* removes this node */
 		list* rm();
 
+		/* constructor */
 		list();
 
 };
