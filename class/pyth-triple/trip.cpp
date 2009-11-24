d9747a76e5700fbb57e1d0c496bc9b57c88293c3
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-24 16:57:43 +0000

    add pyth triple stuff

---
+++ class/pyth-triple/trip.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+/*
+	TODO: make this DP -> store result of multiplication in array
+*/
+int main(void)
+{
+	int a,b,c;
+	int counter;
+	int tripAmt=0;
+	const int num_trips = 20;
+
+	int tripArr[num_trips][3] = {{0},{0},{0}};
+
+	for (c=1; tripAmt < num_trips; ++c)
+	{
+		for (a=1; a <= 2000; ++a)
+		{
+			for (counter=0; counter < tripAmt; ++counter)
+			{
+				/* lets see if we are a duplicate */
+				if (a % tripArr[counter][0] == 0)
+				{
+					/* duplicate found; increase a by one and start over the check */
+					++a;
+					counter=0;
+				}
+			}
+			for (b=a; b<= 2000; ++b)
+			{
+				if (pow(a,2) + pow(b,2)==pow(c,2))
+				{
+					std::cout << a << " + "<< b << " = "<< c << "\n\t";
+					std::cout << pow(a,2) << " + "<< pow(b,2) << " = "<< pow(c,2) << "\n";
+					tripArr[tripAmt][0] = a;
+					tripArr[tripAmt][1] = b;
+					tripArr[tripAmt][2] = c;
+					++tripAmt;
+				}
+			}
+		}
+	}
+	return 0;
+}
