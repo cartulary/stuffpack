03d8cc193c3921b16b991d6c57dc3bfb33ea8fb3
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-24 12:28:42 +0000

    lets get the prime number code into prime3; then we will attempt to solve the question

--- cst/prime3-q18/prime3.cpp
+++ cst/prime3-q18/prime3.cpp
@@ -9,7 +9,13 @@
  *********************************************/
 
 #include <iostream>
+#include <math.h>
+#include <gmp.h>
+#include <gmpxx.h>
+
+
 int sumDigits(int num);
+bool isPrime(int num);
 
 int main (int argc, char* argv[])
 {
@@ -26,4 +32,33 @@ int sumDigits(int num)
 		num /= 10;
 	}
 	return sum;
+}
+
+bool isPrime(int num)
+{
+	/* we only check until the square root */
+	int maxFactor = static_cast<int>(floor(sqrt(num)));
+
+	/* for == 2 and <1 use the "else" */
+	if (num >= 3 && num %2 != 0)
+	{
+		/* check for all odd numbers  */
+		for (int i = 3; i <= maxFactor; i += 2)
+		{
+			/* check to see if we are prime */
+			if (num % i == 0)
+			{
+				/* we are not prime; stop checking */
+				return false;
+			}
+		}
+	}
+	else
+	{
+		if (num != 2)
+		{
+			return false;
+		}
+	}
+	return true;
 }
