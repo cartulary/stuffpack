9b952e06cf2d35fc438fa919362c5261abc2d0b0
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-24 18:40:14 +0000

    start to add some python code; I need to get some practice

---
+++ prime-py/primes.py
@@ -0,0 +1,12 @@
+# We want to test to see if our argument list is prime.
+
+from math import sqrt
+
+def isPrime(num):
+	for i in range(3, num):
+		if num % i == 0:
+			return False
+	return True
+
+print isPrime(11);
+print isPrime(12);
