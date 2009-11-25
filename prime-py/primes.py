3453768660d30e29b9e32deead650651f38e5919
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-25 21:05:16 +0000

    improve isPrime; the argv looping works now

--- prime-py/primes.py
+++ prime-py/primes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S python -tt3
+#!/usr/bin/env -S python -tt3 -Wd
 
 # We want to test to see if our argument list is prime.
 
@@ -6,10 +6,14 @@ from math import sqrt
 import sys;
 
 def isPrime(num):
-	for i in range(3, int(sqrt(num)) + 1):
+	if num < 2:
+		return False;
+	if num % 2 == 0:
+		return False;
+	for i in range(3, num):
 		if num % i == 0:
-			return False
-	return True
+			return False;
+	return True;
 
 for arg in sys.argv:
 	if arg != sys.argv[0]:
