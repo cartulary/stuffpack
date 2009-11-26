8aa8156429bec488275d7c3601b2303c09d37ba3
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-26 12:39:22 +0000

    Lets now include a library of common functions that I wrote or will write; rewrite some programs to work with that library

---
+++ libhello/libhello.h
@@ -0,0 +1,1 @@
+extern bool isPrime(long num);
--- prime/primeA.cpp
+++ prime/primeA.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include "../libhello/libhello.h"
 
 /* only accpet the CLI arguments */
 int main (int argc, char *argv[])
@@ -15,37 +16,8 @@ int main (int argc, char *argv[])
 	/* get the first integer; maybe check them all eventually? */
 	toCheck = atoi(argv[1]);
 
-	/* we only check until the square root */
-	int maxFactor = floor(sqrt(toCheck));
-
-	/* initialize to true unless proven false*/
-	bool isPrime = true;
-
-	/* for == 2 and <1 use the "else" */
-	if (toCheck > 2 && toCheck %2 != 0)
-	{
-		/* check for all odd numbers  */
-		for (int i = 3; i <= maxFactor; i += 2)
-		{
-			/* check to see if we are prime */
-			if (toCheck % i == 0)
-			{
-				isPrime = false;
-				/* we are not prime; stop checking */
-				break;
-			}
-		}
-	}
-	else
-	{
-		isPrime = false;
-		if (toCheck == 2)
-		{
-			isPrime = true;
-		}
-	}
-
+	bool isPrimeResult = isPrime(toCheck);
 	/* print our result -> only print 'npt' if we not prime*/
-	std::cout << ((isPrime) ? "" : "not ") << "prime" << std::endl;
+	std::cout << ((isPrimeResult) ? "" : "not ") << "prime" << std::endl;
 	return 0;
 }
