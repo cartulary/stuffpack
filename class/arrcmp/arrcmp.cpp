5fff6fd59bcf245c9d067f119ea98392949da0f4
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-26 21:24:17 +0000

    add more code for yet more requirements

--- class/arrcmp/arrcmp.cpp
+++ class/arrcmp/arrcmp.cpp
@@ -2,24 +2,37 @@
 
 template <typename arrType>
 bool arrCmp(arrType a1[], arrType a2[], int size);
+bool arrCmp(char a1[], char a2[]);
 
 int main(void)
 {
 	int a1[] = {1,2,3};
 	int a2[] = {1,2,3};
-	std::cout << ((arrCmp(a1,a2,3)==0) ? "false":"true")  << "\n";
+	std::cout << "T=" << ((arrCmp(a1,a2,3)) ? "true":"false")  << "\n";
 
 	char b1[] = {'d','D','e'};
 	char b2[] = {'d','D','e'};
-	std::cout << ((arrCmp(b1,b2,3)==0) ? "false":"true")  << "\n";
+	std::cout << "T=" <<((arrCmp(b1,b2,3)) ? "true":"false")  << "\n";
 
 	int c1[] = {1,2,3};
 	int c2[] = {1,2,4};
-	std::cout << ((arrCmp(c1,c2,3)==0) ? "false":"true")  << "\n";
+	std::cout << "F=" <<((arrCmp(c1,c2,3)) ? "true":"false")  << "\n";
 
 	char d1[] = {'d','D','e'};
 	char d2[] = {'d','D','E'};
-	std::cout << ((arrCmp(d1,d2,3)==0) ? "false":"true")  << "\n";
+	std::cout << "F=" <<((arrCmp(d1,d2,3)) ? "true":"false")  << "\n";
+
+	char e1[] = "abc";
+	char e2[] = "abc";
+	std::cout << "T=" <<((arrCmp(e1,e2)) ? "true":"false")  << "\n";
+
+	char f1[] = "abc";
+	char f2[] = "abcd";
+	std::cout << "F=" << ((arrCmp(f1,f2)) ? "true":"false")  << "\n";
+
+	char g1[] = "abc";
+	char g2[] = "xyz";
+	std::cout << "F=" << ((arrCmp(g1,g2)) ? "true":"false")  << "\n";
 	return 0;
 }
 
@@ -37,5 +50,16 @@ bool arrCmp(arrType a1[], arrType a2[], int size)
 	return true;
 }
 
-void foo (arrType a)
-{}
+bool arrCmp(char a1[], char a2[])
+{
+	int i;
+	for (i=0; a1[i]!=NULL || a2[i] != NULL; ++i)
+	{
+		if (a1[i] != a2[i])
+		{
+			return false;
+		}
+	}
+
+	return (a1[i] == a2[i]);
+}
