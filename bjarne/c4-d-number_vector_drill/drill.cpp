0bff2e9f4b1c3c73cb55afd7d71e819bf9ca680e
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-11-29 16:03:20 +0000

    lets add some of the drill code

---
+++ bjarne/c4-d-number_vector_drill/drill.cpp
@@ -0,0 +1,35 @@
+/**********************************************
+ * Name: drill                                *
+ * Returns: 0 always                          *
+ *********************************************/
+
+#include <iostream>
+#include <sysexits.h>
+#include <iomanip>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+	double num1;
+	double num2;
+	while (1)
+	{
+		cin >> num1 >> num2;
+		if (num2 < num1)
+		{
+			swap(num1, num2);
+		}
+		if (num1==num2)
+		{
+			cout << "They are exactly equal";
+		}
+		else if ((num2 - num1) < (1.0/10000000))
+		{
+			cout << "The number are almost equal\n";
+		}
+		cout << "The smaller value is: " << num1 << "\nThe larger value is: " << num2;
+		cout << "\n";
+	}
+	return EX_OK;
+}
