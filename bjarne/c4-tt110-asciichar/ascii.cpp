507f8022b3cf2a2ff2aac5d7184a12508bc918bd
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-05 21:42:18 +0000

    move on the format wanted by c4-tt112

--- bjarne/c4-tt110-asciichar/ascii.cpp
+++ bjarne/c4-tt110-asciichar/ascii.cpp
@@ -10,13 +10,22 @@
 
 using namespace std;
 
+void printList(int start, int end);
+
 int main (int argc, char* argv[])
 {
 	cout << setw(5) << left << "Int" << setw(5) << "Oct" << "Letter" << "\n";
+	printList('A','Z');
+	printList('a','z');
+	printList('0','9');
+	return EX_OK;
+}
+
+void printList(int start, int end)
+{
 	char letter;
-	for (letter = 'a'; letter <= 'z'; ++letter)
+	for (letter = start; letter <= end; ++letter)
 	{
 		cout << setw(5) << left << static_cast<int>(letter) << setw(5) << oct << static_cast<int>(letter) << letter << "\n";
 	}
-	return EX_OK;
 }
