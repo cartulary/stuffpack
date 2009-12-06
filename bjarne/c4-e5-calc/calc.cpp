e523a367cc6c9f33c5feee46e761eb1c267b575e
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-06 17:57:48 +0000

    Now we can handle a greater amount of cases (not just A op B but include A op B ... op N)

--- bjarne/c4-e5-calc/calc.cpp
+++ bjarne/c4-e5-calc/calc.cpp
@@ -11,39 +11,51 @@
 
 #include <iostream>
 #include <sysexits.h>
+#include <err.h>
 
 using namespace std;
 
 int main (int argc, char* argv[])
 {
 	// Each of the numbers we will be working with
-	double first, second;
+	double lval =0;
+	double rval;
 	// what operation to perform
 	char op;
-	// what to displau...
-	double result;
-	while (1)
+
+	cin >> lval;
+	if (!cin)
+	{
+		errx(EX_NOINPUT,"%s","You didn't enter any data");
+	}
+	// as long as we still have an operator continue to do stuff
+	while (cin >> op)
 	{
 		// get the ultra simple expression
-		cin>> first >> op >> second;
+		cin >> rval;
+		if (!cin)
+		{
+			errx(EX_NOINPUT,"%s", "You didn't enter an rvalue");
+		}
 		// ok - now we decide on a result
 		switch (op)
 		{
 			case '+':
-				result = first + second;
+				lval += rval;
 				break;
 			case '-':
-				result = first - second;
+				lval -= rval;
 				break;
 			case '*':
-				result = first * second;
+				lval *= rval;
 				break;
 			case '/':
-				result = first / second;
+				lval /= rval;
 				break;
+			default:
+				cout << "Result = " << lval << "\n";
+				return EX_OK;
 		}
-		// and print it out
-		cout << "Result = " << result << "\n";
 	}
 	return EX_OK;
 }
