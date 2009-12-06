9cf49b69ea33d2339b6cdd2914cd1083728b8ecc
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-06 20:27:33 +0000

    ok... so we now have some idea of where things are going; I left the old code in for reference

--- bjarne/c4-e5-calc/calc.cpp
+++ bjarne/c4-e5-calc/calc.cpp
@@ -12,8 +12,18 @@
 #include <iostream>
 #include <sysexits.h>
 #include <err.h>
+#include "token.h"
 
 using namespace std;
+
+// Reads characters and composes tokens; use cin
+Token get_token();
+// deals with + and - (pri 2); uses term() and get_token()
+double expression();
+// deals with */% (pri 1); uses primary() and get_token()
+Token term();
+// deals with numbers and () (pri 0); uses expression() and get_token();
+double primary();
 
 int main (int argc, char* argv[])
 {
@@ -23,11 +33,15 @@ int main (int argc, char* argv[])
 	// what operation to perform
 	char op;
 
+	cout << expression();
+	return EX_OK;
+
 	cin >> lval;
 	if (!cin)
 	{
 		errx(EX_NOINPUT,"%s","You didn't enter any data");
 	}
+
 	// as long as we still have an operator continue to do stuff
 	while (cin >> op)
 	{
@@ -58,4 +72,43 @@ int main (int argc, char* argv[])
 		}
 	}
 	return EX_OK;
+}
+
+double expression()
+{
+	// infinite recursion
+	Token l = term();
+	Token t = get_token();
+	switch (l.kind)
+	{
+		case '+':
+			l.value += term().value;
+			break;
+		case '-':
+			l.value -= t.value;
+			break;
+		default:
+			l.value = t.value;
+			//we do nothing... let our calling function deal with that
+	}
+	cout << "(expr) L = " << l.value <<" O = " << t.kind << " R = " << t.value << "\n";
+	return l.value;
+}
+
+Token term()
+{
+	cout << "(term)\n";
+	Token t = get_token();
+	return t;
+}
+
+Token get_token()
+{
+	double rvalue;
+	char op;
+	cin >> rvalue;
+	cin >> op;
+	Token t(op, rvalue);
+	cout << "(tkn) O = " << op <<" R = " << rvalue <<"\n";
+	return t;
 }
