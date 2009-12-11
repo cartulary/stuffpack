d1d46842520dba8788a9ae908453f99b5e9229e6
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-11 09:58:01 +0000

    Lets make this more SCM agnostic - as well as removing the need for many variables in Makefiles

--- ports-svn/svn_create_tar.sh
+++ ports-svn/svn_create_tar.sh
@@ -3,16 +3,21 @@ use_scm="";
 opt_get_head="";
 opt_export="";
 opt_make_tar="";
-while getopts ad:hetV option
+scm_export_args="";
+scm_url="";
+while getopts ad:rshetV option
 do
 	case "$option" in
 		'a') opt_get_head="yes"; opt_export="yes"; opt_make_tar="yes";;
 		'd') port="$OPTARG";;
 		'h') opt_get_head="yes";; #get head
 		'e') opt_export="yes";; #export files
+		'r') svn_rev="$OPTARG";; #revision number
+		's') use_scm="$OPTARG";;
 		't') opt_make_tar="yes";; #tar files
+		'u') scm_url="$OPTARG";;
 		'V') echo "Tool maintained by Eitan Adler (EitanAdlerList@gmail.com). Please contact him with updates, changes, and fixes.";
-			return 0;
+			return 0;;
 	esac
 done
 echo "Dir: $port";
@@ -22,18 +27,24 @@ then
 	cd "$port";
 else
 	echo "This port does not exist; fail!";
+	return 1;
 fi
 
 do_get_head()
 {
-	svn_head=$(svn info $svn_url | grep "Revision" | cut -d' ' -f2);
-	echo "SVN_REV=    $svn_rev" >$mk_masterdir/Makefile.svn_rev;
+	svn_head=$(svn info $scm_url | grep "Revision" | cut -d' ' -f2);
+	if [ -w $mk_masterdir/Makefile.svn_rev ]
+	then
+		echo "SVN_REV=    $svn_rev" >$mk_masterdir/Makefile.svn_rev;
+	else
+		echo "Makefile.svn_rev is not writable. Set SVN_REV to $svn_rev manually";
+	fi	
 }
 
 do_get_src()
 {
 	mkdir $mk_wrkdir;
-	svn export -r $svn_rev $svn_url $mk_wrksrc;
+	$use_scm $scm_export_args $scm_url $mk_wrksrc
 }
 
 do_make_tar()
@@ -43,19 +54,18 @@ do_make_tar()
 }
 
 #can this be done faster? I don't want to have to run make 1000 times ;)
-use_scm=$(make -V USE_SCM);
-svn_rev=$(make -V SVN_REV);
-svn_url=$(make -V SVN_URL);
 mk_wrkdir=$(make -V WRKDIR);
 mk_wrksrc=$(make -V WRKSRC);
 mk_distdir=$(make -V DISTDIR);
 mk_distname=$(make -V DISTNAME);
 mk_masterdir=$(make -V MASTERDIR);
 
-if [ "$use_scm" = "svn" ]
+if [ "$use_scm" !="svn" ]
 then
 	echo "We don't yet handle non svn based projects. This is a future feature";
 	return 1;
+else
+	scm_export_args="export -r $svm_rev"
 fi
 
 if [ -n "$opt_get_head" ]
@@ -72,7 +82,6 @@ fi
 if [ -n "$opt_make_tar" ]
 then
 	do_make_tar;
-	echo "Not making tar";
 fi
 
-echo "Working with $svn_rev AND $svn_url";
+echo "Working with $svn_rev AND $scm_url";
