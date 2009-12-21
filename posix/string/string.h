32137284692c53b1cbe69c5ed1a676a22039cdcf
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-21 12:22:32 +0000

    lets get this working

--- posix/string/string.h
+++ posix/string/string.h
@@ -3,7 +3,7 @@ char* stpcpy(char *dst, const char *src);
 /* POSIX */
 char* strcat(char *s, const char *append);
 /* POSIX */
-//char* strncat(char *s, const char *append, size_t size);
+char* strncat(char *s, const char *append, size_t size);
 /* POSIX */
 char* strchr(const char *s, int c);
 /* POSIX */
@@ -11,27 +11,27 @@ char* strrchr(const char *s, int c);
 /* POSIX */
 int strcmp(const char *s1, const char *s2);
 /* POSIX */
-//int strncmp(const char *s1, const char *s2, size_t size);
+int strncmp(const char *s1, const char *s2, size_t size);
 int strcasecmp(const char *s1, const char *s2);
-//int strncasecmp(const char *s1, const char *s2, size_t size);
+int strncasecmp(const char *s1, const char *s2, size_t size);
 /* POSIX */
 char* strcpy(char *dst, const char *src);
 /* POSIX */
-//char * strncpy(char *dst, const char *src, size_t size);
+char * strncpy(char *dst, const char *src, size_t size);
 /* POSIX */
 char * strerror(int errno);
 /* POSIX */
-//size_t strlen(const char *s);
+size_t strlen(const char *s);
 /* POSIX */
 /* Will not be implemented for now */
 char* strpbrk(const char *s, const char *charset);
 char* strsep(char **stringp, const char *delim);
 /* POSIX */
 /* Will not be implemented for now */
-//size_t strspn(const char *s, const char *charset);
+size_t strspn(const char *s, const char *charset);
 /* POSIX */
 /* Will not be implemented for now */
-//size_t  strcspn(const char *s, const char *charset);
+size_t  strcspn(const char *s, const char *charset);
 /* POSIX */
 char * strstr(const char *s1, const char *s2);
 /* POSIX */
