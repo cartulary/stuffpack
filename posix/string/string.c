daba0b87cc975e66f961107180340149206f9690
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-21 12:10:30 +0000

    lets commit the header file... we will soon copy the work from class of strcmp and I'll continue my rewrite

---
+++ posix/string/string.c
@@ -0,0 +1,7 @@
+#include <sysexits.h>
+#include "./string.h"
+#include <stddef.h>
+int main(void)
+{
+	return EX_OK;
+}
---
+++ posix/string/string.h
@@ -0,0 +1,41 @@
+#include <stddef.h>
+char* stpcpy(char *dst, const char *src);
+/* POSIX */
+char* strcat(char *s, const char *append);
+/* POSIX */
+//char* strncat(char *s, const char *append, size_t size);
+/* POSIX */
+char* strchr(const char *s, int c);
+/* POSIX */
+char* strrchr(const char *s, int c);
+/* POSIX */
+int strcmp(const char *s1, const char *s2);
+/* POSIX */
+//int strncmp(const char *s1, const char *s2, size_t size);
+int strcasecmp(const char *s1, const char *s2);
+//int strncasecmp(const char *s1, const char *s2, size_t size);
+/* POSIX */
+char* strcpy(char *dst, const char *src);
+/* POSIX */
+//char * strncpy(char *dst, const char *src, size_t size);
+/* POSIX */
+char * strerror(int errno);
+/* POSIX */
+//size_t strlen(const char *s);
+/* POSIX */
+/* Will not be implemented for now */
+char* strpbrk(const char *s, const char *charset);
+char* strsep(char **stringp, const char *delim);
+/* POSIX */
+/* Will not be implemented for now */
+//size_t strspn(const char *s, const char *charset);
+/* POSIX */
+/* Will not be implemented for now */
+//size_t  strcspn(const char *s, const char *charset);
+/* POSIX */
+char * strstr(const char *s1, const char *s2);
+/* POSIX */
+char * strtok(char *s, const char *delim);
+char * index(const char *s, int c);
+char * rindex(const char *s, int c);
+ 
