81d825189d4b9c21d68f39f1cfa14d63a454ab0c
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2009-12-21 12:18:21 +0000

    make the code a tiny bit less repetitive

--- class/strcmp/strcmp.cpp
+++ class/strcmp/strcmp.cpp
@@ -101,7 +101,7 @@ int mycmp(const char* s1, const char* s2)
 int myncmp(const char* s1, const char* s2, size_t len)
 {
 	int pos;
-	for (pos = 0; s1[pos] != '\0' && s2[pos] != '\0' && pos < len; ++pos)
+	for (pos = 0; (s1[pos] != '\0' || s2[pos] != '\0') && pos < len; ++pos)
 	{
 //		std::cerr << "pos=" << pos << "len=" << len << "s1/s2" << s1[pos] << " " << s2[pos] <<"\n";
 		if (s1[pos] != s2[pos])
@@ -115,5 +115,5 @@ int myncmp(const char* s1, const char* s2, size_t len)
 	{
 		++pos;
 	}
-	return s1[pos] - s2[pos];
+	return 0;
 }
