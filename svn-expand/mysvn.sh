22bc1110cca6ad240c91f8bef1bf953c2014609c
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-01-07 17:30:48 +0000

    we want to add svn alias

---
+++ svn-expand/mysvn.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+regex="^(add|commit|import|(merge)?info|prop(list|del|edit|get|set)|blame|copy|sw(itch)?|mkdir|(un)?lock|cat|delete|list|move|resolve|up(date)?|changelist|checkout|c(l|o|i|p)|di|p(d|e|g|l|s)|st|cleanup|diff|export|help|lock|merge|resolved|revert|stat(us)?|praise|annotate|ann|cl|cp|del|ls|h|\?)$";
+realsvn=$(printf "%s" "$1" |grep -Ec "$regex")
+#echo $realsvn;
+#return 0;
+
+if [ $realsvn -eq 1 ]
+then
+	\svn $@;
+	return $?;
+fi
+case "$1" in
+	"ohelp")
+		echo "no help currently available";;
+	"grep")
+		#move everthing over to get rid of "svn" and then ask grep to help us
+		shift 1;
+		grep --exlude "*svn*" ;;
+	*)
+		#since we have an invalid command lets call the real svn -> get a usage message -> then print our extension
+		\svn $@;
+		echo "--> Also valid: ohelp";;
+esac
+
