9e2d21d3b1be1a4ed26f8c65d8c990fd5ddef4c5
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-01-25 14:04:33 +0000

    Now we got a new class that isn't being used - lets work on it

--- ds/fifo/lifo.cpp
+++ ds/fifo/lifo.cpp
@@ -8,33 +8,15 @@ Lifo::Lifo() : head(NULL)
 
 int Lifo::pop()
 {
-	Node* current = head;
-	Node* prev = NULL;
-	if (current == NULL)
+	if (head == NULL)
 	{
 		return -1;
 		//throw exception
 	}
-	while (current->next)
-	{
-		prev = current;
-		current = current->next;
-	}
-	int ret = current->data;
-	if (prev)
-	{
-		delete current;
-		prev->next= NULL;
-	}
-	/*
-		this is for a LIFO
-	*/
-	/*
 	int ret = head->data;
 	Node* tmp = head->next;
 	delete tmp;
 	head = tmp;
-	*/
 	return ret;
 
 }
