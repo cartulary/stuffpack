05392f9d87d28fd48401f9bc21c3fcaeed7dd556
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-01-25 15:30:03 +0000

    right now non-popping printing is messed up

--- ds/fifo/fifo.cpp
+++ ds/fifo/fifo.cpp
@@ -3,6 +3,10 @@
 #include <cstdio>
 #include <iostream>
 
+Fifo::Fifo() : numNodes(0)
+{
+}
+
 int Fifo::pop()
 {
 	Node* current = head;
@@ -23,6 +27,29 @@ int Fifo::pop()
 		delete current;
 		prev->next= NULL;
 	}
+	--numNodes;
 	return ret;
 
+}
+
+void Fifo::push(int data)
+{
+	Fo::push(data);
+	++numNodes;
+}
+
+Node* Fifo::getNextNode(int fastfoward)
+{
+	int count = numNodes - fastfoward;
+	Node* current = head;
+	if (current == NULL)
+	{
+		return NULL;
+	}
+	while (current->next && count>=0)
+	{
+		count--;
+		current = current->next;
+	}
+	return current;
 }
