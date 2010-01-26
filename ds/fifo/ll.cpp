f07b75696fac57aa95cfaaf6fbc61c77047d3794
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-01-26 20:57:41 +0000

    now we just need to implement deleting item

--- ds/fifo/ll.cpp
+++ ds/fifo/ll.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
+#include "printit.h"
 #include "ll.h"
-Ll::Ll() : head(NULL)
+Ll::Ll() : head(NULL), numnodes(0)
 {
 
 }
@@ -11,24 +13,43 @@ Ll::~Ll()
 
 void Ll::push(int data, int loc)
 {
-	/* This assumes that loc = 0
-	 *
-	 */
 	++numnodes;
-	if (head == NULL)
+	if (!head)
 	{
 		head = new Node<int>(data);
 		return;
 	}
 	Node<int>* current = head;
-	if (current != NULL)
+	if (loc == -1)
 	{
-		while (current->next != NULL)
+		while (current->next)
 		{
-			current=current->next;
+			current = current->next;
 		}
+		current->next = new Node<int>(data);
 	}
-	current->next = new Node<int>(data);
+	else
+	{
+		/* if we have a total of one item */
+		if (!current->next)
+		{
+			current->next = new Node<int>(data);
+			return;
+		}
+		while (current->next->next)
+		{
+			int count = loc;
+			--count;
+			current=current->next;
+			if (count == 0)
+			{
+				break;
+			}
+		}
+		Node<int>* tmp = current->next;
+		current->next = new Node<int>(data);
+		current->next->next = tmp;
+	}
 	/* we want to tell our new node who we are */
 
 }
@@ -36,7 +57,7 @@ void Ll::push(int data, int loc)
 int Ll::read(int loc)
 {
 	int count = loc;
-	Node<int>* current;
+	Node<int>* current = head;
 	if (!current)
 	{
 		//actually throw an exception
@@ -45,10 +66,35 @@ int Ll::read(int loc)
 	while (current->next)
 	{
 		count--;
-		if (!count)
+		if (count == 0)
 		{
 			break;
 		}
+		current = current->next;
 	}
 	return current->data;
+}
+
+std::vector<int> Ll::getAllNodes()
+{
+	std::vector<int> ret;
+	Node<int>* current = head;
+	if (!current)
+	{
+		return ret;
+	}
+	while (current->next)
+	{
+		std::cout << "pushing next in getAllNodes\n";
+		ret.push_back(current->data);
+		current = current->next;
+	}
+	ret.push_back(current->data);
+}
+
+void Ll::printAll()
+{
+	std::vector<int> vals = getAllNodes();
+	Printit<int> doprint;
+	std::for_each(vals.begin(), vals.end(), doprint);
 }
