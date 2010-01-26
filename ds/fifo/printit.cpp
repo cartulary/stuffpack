fb10c5f8a3e01492ea70e3e5eb91c8fdb4992bab
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-01-26 12:59:31 +0000

    make printAll work correctly in all cases

--- ds/fifo/printit.cpp
+++ ds/fifo/printit.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "printit.h"
-   void Printit::operator() (int i)
-   {
-       std::cout << i <<' ';
-   }
+void Printit::operator() (int i)
+{
+    std::cout << i <<' ';
+}
