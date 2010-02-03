0c5016eefc26714cfdaff82e9c90d91f728457c0
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-02-03 22:13:48 +0000

    Lets create the node and some basic tests

---
+++ ds/btree/dotest.cpp
@@ -0,0 +1,43 @@
+#include "dotest.h"
+
+MultiNode* t_mn;
+
+void test_node_ptrs(void)
+{
+	t_mn = new MultiNode(0,2);
+	CU_ASSERT_PTR_NULL(t_mn->ptrs[0]);
+	CU_ASSERT_PTR_NULL(t_mn->ptrs[1]);
+}
+
+void test_node_data(void)
+{
+}
+
+
+int doTest(void)
+{
+	/* create the registry */
+	if (CUE_SUCCESS != CU_initialize_registry())
+	{
+		return CU_get_error();
+	}
+
+	CU_TestInfo test_array_node[] = {
+		{ "node accepts multiple pointers correctly", test_node_ptrs },
+		{ "node accepts data correctly", test_node_data },
+	  	CU_TEST_INFO_NULL,
+	};
+
+	CU_SuiteInfo suites[] = {
+	  { "node", NULL, NULL, test_array_node },
+	  CU_SUITE_INFO_NULL,
+	};
+
+	CU_ErrorCode error = CU_register_suites(suites);
+
+	/* Run all tests using the CUnit Basic interface */
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	CU_cleanup_registry();
+	return CU_get_error();
+}
