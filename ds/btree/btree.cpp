8281460d088577b9825cddd921b47c6588000ada
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-02-04 10:57:23 +0000

    add an enum for the more and less pointer; lets add an add implemtation;

--- ds/btree/btree.cpp
+++ ds/btree/btree.cpp
@@ -6,6 +6,42 @@ BinaryTree::BinaryTree(): head(NULL)
 
 void BinaryTree::add(int data)
 {
+	/*
+		If head is null just stick the data into head.
+		Else
+			if data < head->data
+				attempt add and proper location or loop
+			else if data > head->data
+				attempt add and proper location or loop
+			else
+				throw exception dup data
+	*/
+	if (!head)
+	{
+		head = new MultiNode(2,data);
+		return;
+	}
+	else
+	{
+		MultiNode* current = head;
+		while (current)
+		{
+			if (data < current->data)
+			{
+				current = current->ptrs[LESS_PTR];
+			}
+			else if (data > current->data)
+			{
+				current = current->ptrs[MORE_PTR];
+			}
+			else
+			{
+				//throw duplicateData exception
+				return;
+			}
+		}
+		return;
+	}
 }
 
 void BinaryTree::remove(int data)
--- ds/btree/btree.h
+++ ds/btree/btree.h
@@ -5,6 +5,7 @@ class BinaryTree {
 	private:
 		unsigned int numnodes;
 		MultiNode* head;
+		enum { LESS_PTR, MORE_PTR };
 	public:
 		BinaryTree();
 		//~BinaryTree();
