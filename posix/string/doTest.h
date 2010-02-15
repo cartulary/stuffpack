d2ecbd1b3a0ee381d3f66e331f83b290d146eec6
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-02-15 14:54:55 +0000

    Introduce header files

---
+++ posix/string/doTest.h
@@ -0,0 +1,29 @@
+#ifdef REAL_STRING
+    #include <string.h>
+#else
+    #include "./string.c"
+#endif
+
+#include <sysexits.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+
+void test_strlen_strings(void);
+void test_strlen_emptystring(void);
+void test_strlen_multinull(void);
+
+void test_strnlen_greater(void);
+void test_strnlen_equeal(void);
+void test_strnlen_lesser(void);
+void test_strnlen_zero(void);
+void test_strnlen_neg(void);
+
+void test_strcmp_match(void);
+void test_strcmp_match(void);
+void test_strncmp_nomatch(void);
+void test_strncmp_nomatch(void);
+
+void test_strcasecmp_match(void);
+void test_strcasecmp_mismatch(void);
+void test_strcasecmp_mismatchcase(void);
