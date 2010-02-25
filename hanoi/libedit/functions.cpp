d873378578659ae36d79dbfdeba0eca8d24e479d
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-02-25 16:43:42 +0000

    I made a few changes - I'm upgrading hanoi now

---
+++ hanoi/libedit/functions.cpp
@@ -0,0 +1,33 @@
+#include "../functions.h"
+
+/* Retrieves a positive integer from the user. */
+long int getinteger(int base)
+{
+  	int n = 0;
+
+  	cout << "Please enter a positive integer:" << endl;
+  	n = strtol(getcharcters(10),NULL,10);
+
+  	if (n <= 0)
+  	{
+  		n = getinteger(base);
+  	}
+    	return n;
+}
+
+/* Retrieves character input from the user, up to the specified maximum quantity. */
+char* getcharcters(int max)
+{
+	char* str = (char*)calloc(max, sizeof(int));
+	char ch;
+	int i = 0;
+
+	while ((ch = getchar()) != '\n' && i < max)
+	{
+		str[i] = ch;
+		i++;
+	}
+
+	str[max] = NULL; //I hope this is the right way to terminate a char array.
+	return str;
+}
---
+++ hanoi/libedit/main.cpp
@@ -0,0 +1,29 @@
+//Includes
+#include <stdio.h>
+#include <string>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "functions.cpp"
+#include "../hanoi.cpp"
+
+int main (int argc, char *argv[])
+{
+	long n = 0; //The amount of blocks.
+	/* If the amount of blocks was passed through a command line argument use it: */
+	if (argc > 1)
+	{
+		n = strtol(argv[1],NULL,10); //Extract the # of blocks from CLI arguments and assign it to n.
+		/* If n has an incorrect value, ask the user to input a correct one: */
+	}
+	/* If no number of blocks was passed via command line, ask the user to input a number: */
+	/* or if there was an error with the initial input */
+	while (n <= 0)
+	{
+		n = getinteger(10);
+	}
+
+	Hanoi(n, 'a','b','c');
+	return 0;
+}
