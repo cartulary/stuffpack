ff625f17cb0811359f7f08fca40bb0f313b81487
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-02-25 17:11:22 +0000

    Learning how to use editline

--- hanoi/libedit/functions.cpp
+++ hanoi/libedit/functions.cpp
@@ -1,33 +1,50 @@
 #include "../functions.h"
+#include <histedit.h>
 
-/* Retrieves a positive integer from the user. */
-long int getinteger(int base)
-{
-  	int n = 0;
-
-  	cout << "Please enter a positive integer:" << endl;
-  	n = strtol(getcharcters(10),NULL,10);
-
-  	if (n <= 0)
-  	{
-  		n = getinteger(base);
-  	}
-    	return n;
+const char* prompt(EditLine *e);
+const char* prompt(EditLine *e) {
+	return "Enter a number: ";
 }
 
-/* Retrieves character input from the user, up to the specified maximum quantity. */
-char* getcharcters(int max)
+long int getinteger(int base)
 {
-	char* str = (char*)calloc(max, sizeof(int));
-	char ch;
-	int i = 0;
+	EditLine *el;
+	History *hist;
 
-	while ((ch = getchar()) != '\n' && i < max)
+	int count;
+	const char* line;
+	bool gotint = false;
+	HistEvent ev;
+
+	el = el_init("hanoi", stdin, stdout, stderr);
+	el_set(el, EL_PROMPT, &prompt);
+	el_set(el, EL_EDITOR, "emacs");
+
+	hist = history_init();
+	if (!hist)
 	{
-		str[i] = ch;
-		i++;
+		std::cerr << "Sorry - no history\n";
 	}
 
-	str[max] = NULL; //I hope this is the right way to terminate a char array.
-	return str;
+	history(hist, &ev, H_SETSIZE, 5);
+
+	el_set(el, EL_HIST, history, hist);
+
+	int n;
+	while (!gotint)
+	{
+		line = el_gets(el, &count);
+
+		if (count > 0)
+		{
+			history(hist, &ev, H_ENTER, line);
+		}
+		n = strtol(line,NULL,base);
+		if (n  >  0)
+		{
+			gotint=true;
+		}
+	}
+
+	return n;
 }
