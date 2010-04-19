a023fe7fd38b74ca92c8cd1cf70c42bbf9c44dc2
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-04-19 12:04:26 +0000

    add the ability to lose; ignore newlines in loss

--- game-a-day/hangman/hangman.java
+++ game-a-day/hangman/hangman.java
@@ -2,6 +2,8 @@ import static java.lang.System.out; import java.io.IOException; import java.util
 
 class hangman {
     public static void main(String[] args) {
+		final int maxBadGuess = 6;
+
 		String[] words = { "apple", "bannana", "power", "reality", "computer" };
 
 		Random randomGenerator = new Random();
@@ -11,14 +13,29 @@ class hangman {
 		String word = words[wordID];
 		out.println("The word is: " + word);
 		Character c = 0;
+		final Character newline = '\n';
+		int badGuesses = 0;
 		try {
 			while(!gameover) {
  				c = (char)System.in.read();
+				if (newline.charValue() == c)
+				{
+					continue;
+				}
 				Character guess = c;
 				guessed.add(c);
 				if (checkWin(word,guessed))
 				{
 					out.println("You won!");
+					gameover = true;
+				}
+				if (-1 == word.indexOf(c))
+				{
+					++badGuesses;
+				}
+				if (maxBadGuess == badGuesses)
+				{
+					out.println("You lost");
 					gameover = true;
 				}
 			}
