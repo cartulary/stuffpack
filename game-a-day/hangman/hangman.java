2a1d5b6971d5ae04f1ed846bc36c295d0603c076
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-04-19 11:55:39 +0000

    Basics of hangman go in now... next implement losing and the graphical board

--- game-a-day/hangman/hangman.java
+++ game-a-day/hangman/hangman.java
@@ -1,5 +1,44 @@
+import static java.lang.System.out; import java.io.IOException; import java.util.*;
+
 class hangman {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+		String[] words = { "apple", "bannana", "power", "reality", "computer" };
+
+		Random randomGenerator = new Random();
+		int wordID = randomGenerator.nextInt(words.length);
+		List<Character> guessed = new ArrayList<Character>();
+		Boolean gameover = false;
+		String word = words[wordID];
+		out.println("The word is: " + word);
+		Character c = 0;
+		try {
+			while(!gameover) {
+ 				c = (char)System.in.read();
+				Character guess = c;
+				guessed.add(c);
+				if (checkWin(word,guessed))
+				{
+					out.println("You won!");
+					gameover = true;
+				}
+			}
+		}
+		catch (IOException e) {
+			out.println("Something bad happend:" + e.getMessage());
+			out.println("Caused by: " + e.getCause());
+		}
+
     }
+
+	private static Boolean checkWin(String word, List<Character> guesses)
+	{
+		for (Character c : word.toCharArray())
+		{
+			if (!guesses.contains(c))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
