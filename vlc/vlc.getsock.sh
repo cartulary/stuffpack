d723dd06174828606c115ebd59f347e8c69bec83
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-05-18 13:32:19 +0000

    If the current socket has to be destroyed - due to the way that the socket name is populated - the old socket name is likely to still exist in the current_sock variable. vlc.getsock.sh needs to clear memory and it must be rerun to fix this problem; race conditions can occur - some sort of locking mechanism needs to be implemented in vlc_create_sock

--- vlc/vlc.getsock.sh
+++ vlc/vlc.getsock.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 optdir="/home/eitan/.vlc_extra";
+current_sock="";
 . "$optdir/socket_name";
 #echo current socket is $current_sock;
 
 isrunning()
 {
-	if [ -e "$current_sock" ]
+	if [ -n "$current_sock" -a -e "$current_sock" ]
 	then
 		return $(true);
 	else
