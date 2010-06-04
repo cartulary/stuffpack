033d15f68d44343480a86fa5f36328e1eb3d02a4
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-06-04 15:35:52 +0000

    I've pretty much got my design down - my major issue now is getting the event handling done correctly

---
+++ game-a-day/ttt/Game.java
@@ -0,0 +1,22 @@
+package ttt;
+import static java.lang.System.out;
+import java.io.IOException;
+import java.util.*;
+
+public class Game implements ButtonPressed {
+
+	public TttBoard myboard;
+  public Game() {
+		myboard = new TttBoard(this);
+		AskAgain asker = new AskAgain();
+//		myboard.disableButton(1);
+/*		while (asker.ask())
+		{}*/
+	}
+
+	public void didPress(int id)
+	{
+		System.out.println(id);
+		myboard.setButton(id, Piece.X);
+	}
+}
--- game-a-day/ttt/Ttt.java
+++ game-a-day/ttt/Ttt.java
@@ -1,12 +1,10 @@
-package ttt;
-import static java.lang.System.out;
-import java.io.IOException;
-import java.util.*;
+import ttt.*;
 
 class Ttt {
-  public static void main(String[] args) {
-		TttBoard myboard = new TttBoard();
-		myboard.setSpot(1,1,Piece.X);
-		myboard.printBoard();
-	}
+
+public static void main(String args[])
+{
+	Game g = new Game();
+}
+
 }
