9f4ec14ab5ede808da33baff718b8998ac00239e
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-06-25 21:31:21 +0000

    some more functions that were missing

--- game-a-day/ttt-cpp/board.cpp
+++ game-a-day/ttt-cpp/board.cpp
@@ -1,24 +1,19 @@
-#include "board.h"
-#include "piece.h"
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.h>
 #include <iostream>
-
+#include "board.h"
+#include "piece.h"
 using namespace ttt;
 
-
 extern Board foo;
-void button_cb(Fl_Widget* w, void* d)
-{
-	w->label(PieceToString(X));
-	foo.setSpot(3,X);
-}
+#include "callback.cpp"
 
 Board::Board()
 {
 	char * x[] = {""};
+	int const nums[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8};
 	Fl_Window *window = new Fl_Window(WINDOW_SIZE,WINDOW_SIZE);
 		int count = 0;
 		buttons[count++] = new Fl_Button(1, 1, BUTTON_SIZE, BUTTON_SIZE, PieceToString(NONE));
@@ -36,7 +31,9 @@ Board::Board()
 		int i;
 		for (i=0; i<count; ++i)
 		{
-			buttons[i]->callback(button_cb, NULL);
+			buttons[i]->callback(button_cb, (void*)(&nums[i]));
+			//for the users start at 1 ;)
+			buttons[i]->shortcut('1'+i);
 		}
 
  	window->end();
@@ -48,3 +45,19 @@ void Board::setSpot(const int id, const Piece p)
 {
 	buttons[id]->label(PieceToString(p));
 }
+
+void Board::disableSpot(const int id)
+{
+	buttons[id]->deactivate();
+}
+
+void Board::enableSpot(const int id)
+{
+	buttons[id]->activate();
+}
+
+Piece Board::getSpot(const int id) const
+{
+	return StringToPiece(buttons[id]->label());
+}
+
