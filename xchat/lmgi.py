40d00878a0f17e9d7c0871153327d946afe89a7d
eitan.adler@gmail.com <eitan.adler@gmail.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-06-29 20:15:01 +0000

    add sanity test for lmgi

--- xchat/lmgi.py
+++ xchat/lmgi.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+
 __module_name__ = "let me google that for you (/lmgi)"
-__module_version__ = "0.8"
+__module_version__ = "0.9"
 __module_description__ = "Operates google for you"
 
 import xchat
@@ -7,10 +9,13 @@ import urllib
 
 print "\0034",__module_name__, __module_version__,"loading...\003"
 def letmegoogleit(word, word_eol, userdata):
+	if len(word) < 2:
+		print ("Choose something to google for");
+		return;
 	wordStr = "+".join(word[1:])
 	print "googling for "+wordStr
 	endUrl = urllib.urlopen('http://tinyurl.com/api-create.php?url=http://letmegooglethatforyou.com?q='+wordStr).read()
 	print endUrl
 	xchat.command("PRIVMSG " +  xchat.get_info("channel") + " " + endUrl)
 
-xchat.hook_command('lmgi', letmegoogleit)
+xchat.hook_command('lmgi', letmegoogleit, help="pick your search terms and type /lmgi terms")
