8f2b7aae2c9bdca31f9dd8ae3715ea1d0bf14e07
eitan@eitanadler.com <eitan@eitanadler.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-10-17 16:32:03 +0000

    add bing for you

--- xchat/allforyou.py
+++ xchat/allforyou.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-__module_name__ = "let me search that for you (/lmgi or /ddg)"
-__module_version__ = "0.9"
+__module_name__ = "let me search that for you (/lmgi /ddg /bing)"
+__module_version__ = "1.0"
 __module_description__ = "Operates search engines for you"
 
 import xchat
@@ -10,6 +10,7 @@ import urllib
 TINYURL_API_URL="http://tinyurl.com/api-create.php?url=[url]"
 GOOGLE_FOR_YOU="http://letmegooglethatforyou.com?q=[term]"
 DDG_FOR_YOU="http://lmddgtfy.com/?q=[term]"
+BING_FOR_YOU="http://www.letmebingthatforyou.com/?q=[term]"
 
 print "\0034",__module_name__, __module_version__,"loading...\003"
 
@@ -36,6 +37,10 @@ def letmegoogleit(word, word_eol, userdata):
 def letmeddgit(word, word_eol, userdata):
 	doWork(word, DDG_FOR_YOU)
 
+def letmebingit(word, word_eol, userdata):
+	doWork(word, BING_FOR_YOU)
+
 
 xchat.hook_command('lmgi', letmegoogleit, help="pick your search terms and type /lmgi terms")
 xchat.hook_command('ddg', letmeddgit, help="pick your search terms and type /ddg terms")
+xchat.hook_command('bing', letmebingit, help="pick your search terms and type /bing terms")
