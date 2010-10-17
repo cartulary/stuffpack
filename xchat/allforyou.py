b4ee7e5c327d068b11e9a1dd16648e8257f2149b
eitan@eitanadler.com <eitan@eitanadler.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-10-17 18:30:44 +0000

    add jfgi and searchbar it (although I'm not sure what that is ;-)

--- xchat/allforyou.py
+++ xchat/allforyou.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
-__module_name__ = "let me search that for you (/lmgi /ddg /bing)"
+__module_name__ = "let me search that for you (/lmgi /ddg /bing /jgi /sbit)"
 __module_version__ = "1.0"
 __module_description__ = "Operates search engines for you"
 
 import xchat
+import string
 import urllib
 
+
+#Url Shortners
 TINYURL_API_URL="http://tinyurl.com/api-create.php?url=[url]"
+
+#Let me * it for you sites
 GOOGLE_FOR_YOU="http://letmegooglethatforyou.com?q=[term]"
 DDG_FOR_YOU="http://lmddgtfy.com/?q=[term]"
 BING_FOR_YOU="http://www.letmebingthatforyou.com/?q=[term]"
+SEARCHBAR_FOR_YOU="http://lmsbtfy.com/?q=[term]"
+JUST_GOOGLE_FOR_YOU="http://www.justfuckinggoogleit.com/search.pl?query=[term]"
+
+#other constants
+HELP_TEXT = "pick your search terms and type /[site] terms"
 
 print "\0034",__module_name__, __module_version__,"loading...\003"
 
@@ -24,6 +34,7 @@ def doWork(word, searchEngine):
 		print ("Choose something to search for");
 		return;
 	wordStr = "+".join(word[1:])
+	wordStr = string.strip(wordStr)
 	print "serching for "+wordStr
 	tinyUrl=getTinyUrl(searchEngine,wordStr,TINYURL_API_URL)
 	endUrl = urllib.urlopen(tinyUrl).read()
@@ -40,7 +51,14 @@ def letmeddgit(word, word_eol, userdata):
 def letmebingit(word, word_eol, userdata):
 	doWork(word, BING_FOR_YOU)
 
+def letmesbit(word, word_eol, userdata):
+	doWork(word, SEARCHBAR_FOR_YOU)
 
-xchat.hook_command('lmgi', letmegoogleit, help="pick your search terms and type /lmgi terms")
-xchat.hook_command('ddg', letmeddgit, help="pick your search terms and type /ddg terms")
-xchat.hook_command('bing', letmebingit, help="pick your search terms and type /bing terms")
+def justGoogleIt(word, word_eol, userdata):
+	doWork(word, JUST_GOOGLE_FOR_YOU)
+
+xchat.hook_command('lmgi', letmegoogleit, HELP_TEXT.replace("[site]","lmgi"))
+xchat.hook_command('ddg', letmeddgit, HELP_TEXT.replace("[site]","ddg"))
+xchat.hook_command('bing', letmebingit, HELP_TEXT.replace("[site]","bing"))
+xchat.hook_command('sbit', letmesbit, HELP_TEXT.replace("[site]","sbit"))
+xchat.hook_command('jgi', justGoogleIt, HELP_TEXT.replace("[site]","jgi"))
