cab9225bc0bfa1b2d94b1c4ce0a43842b49ea460
eitan@eitanadler.com <eitan@eitanadler.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2010-10-18 18:44:55 +0000

    a) add git support b) path name is too long so limit it to 30 chars c) setCurrentPS1 is called by precmd - so we don't need chpwd anymore. also - things could change as a result of a command anyways

--- zsh/ps1.zsh
+++ zsh/ps1.zsh
@@ -1,5 +1,12 @@
 __vcs_dir() {
 	local vcs ref
+	git_dir() {
+  		git rev-parse HEAD 2>/dev/null|| return 1
+
+ 		ref=$(git symbolic-ref -q HEAD || git name-rev --name-only HEAD 2>/dev/null)
+		ref=${ref#refs/heads/}
+		vcs="git"
+	}
 	hg_dir() {
 		hg branch 1>/dev/null 2>/dev/null || return 1;
 
@@ -13,7 +20,7 @@ __vcs_dir() {
         	ref=$(svn info . | awk '/^Revision/ { sub("[^0-9]*","",$0); print $0}')
         	vcs="svn"
       }
-  	svn_dir || hg_dir ||  echo "";
+  	svn_dir || hg_dir || git_dir ||  echo "";
 	echo "${vcs:+[$vcs} ${vcs:+$ref]}"
 }
 
@@ -21,10 +28,10 @@ __vcs_dir() {
 PS1_USER="%F{blue}%n";			#my username
 PS1_HOST="%F{blue}%m";			#hostname
 PS1_ERR="%F{red}%(?..!%?!)";  		#return code of last command (if it was not 0)
-PS1_WD="%F{magenta}%35<...<%~";	#current working directory
+PS1_WD="%F{magenta}%30<...<%~";	#current working directory limited to 30 chars
 PS1_PROMPT="%#";					#EOF
 
-function chpwd()
+function setCurrentPS1()
 {
 	PS1_VCS="%F{green}$(__vcs_dir)";	#info about the vcs
 #	PS1_VCS="%(1v.%F{green}%1v%f.)";		
