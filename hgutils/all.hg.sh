922495588b4a1a7b51552b15719a8db46fcbd053
eitan@eitanadler.com <eitan@eitanadler.com@9a97d292-c974-11dd-b6f5-21ad4398c266>
2011-02-22 22:05:28 +0000

    add timeout part to script

--- hgutils/all.hg.sh
+++ hgutils/all.hg.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
+. ~/.conf/zsh/eval_spin.func
 for remote in $(<.remotes cat)
 do
-	hg "$@" "$remote";
+	echo "=== $remote ===";
+	spin gtimeout 20 hg "$@" "$remote"; 
+	if [ $? -eq 124 ]
+	then	
+		echo "command timed out" > /dev/stderr
+	fi
 done
